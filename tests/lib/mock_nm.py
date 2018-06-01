ea760cb2acb94aaef316a608d377fc6f9cb4ae8e
Till Maas <opensource@till.name>
2018-06-01 15:18:56 +0200

    Support writing IPv4 addresses

--- tests/lib/mock_nm.py
+++ tests/lib/mock_nm.py
@@ -1,5 +1,22 @@
-#!/usr/bin/python3 -tt
-# vim: fileencoding=utf8
+#
+# Copyright 2018 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
 
 UP = 1
 DOWN = 0
@@ -10,6 +27,9 @@ NM_DEVICE_TYPE_GENERIC = 14
 class MockNmConnection(object):
     def get_ip4_config(self):
         return None
+
+    def get_connection(self):
+        return MockNmConnection()
 
 
 class MockNmDevice(object):
