72dc97713c371af7c383e49a5e992fb0e27945ea
Edward Haas <edwardh@redhat.com>
2018-06-03 16:50:49 +0300

    libnmstate: Add support for bond reporting
    The bond interface type is now supported through the reports.
    
    It is introduced with a new nm (networkmanager) package which will act
    as the NM provider, linking between the nmstate business logic and the
    nm api.

---
+++ libnmstate/nm/translator.py
@@ -0,0 +1,59 @@
+#
+# Copyright 2018 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+#
+
+import six
+
+from libnmstate import nmclient
+
+
+IFACE_TYPE_UNKNOWN = 'unknown'
+
+
+def api2nm_iface_type(name):
+    return _get_api2nm_iface_type_map().get(name, IFACE_TYPE_UNKNOWN)
+
+
+def nm2api_iface_type(name):
+    return _get_nm2api_iface_type_map().get(name, IFACE_TYPE_UNKNOWN)
+
+
+def _swap_dict_keyval(dictionary):
+    return {val: key for key, val in six.viewitems(dictionary)}
+
+
+_api2nm_iface_types = None
+_nm2api_iface_types = None
+
+
+def _get_api2nm_iface_type_map():
+    global _api2nm_iface_types
+    if _api2nm_iface_types is None:
+        _api2nm_iface_types = {
+            'ethernet': nmclient.NM.SETTING_WIRED_SETTING_NAME,
+            'bond': nmclient.NM.SETTING_BOND_SETTING_NAME,
+        }
+    return _api2nm_iface_types
+
+
+def _get_nm2api_iface_type_map():
+    global _nm2api_iface_types
+    if _nm2api_iface_types is None:
+        _nm2api_iface_types = _swap_dict_keyval(_get_api2nm_iface_type_map())
+    return _nm2api_iface_types
--- tests/lib/netinfo_test.py
+++ tests/lib/netinfo_test.py
@@ -25,28 +25,42 @@ from libnmstate import netinfo
 
 
 NM_DEVICE_STATE_UNKNOWN = 0
+NM_DEVICE_TYPE_BOND = 10
 NM_DEVICE_TYPE_GENERIC = 14
 
 
 @mock.patch.object(netinfo.nmclient, 'NM')
-@mock.patch.object(netinfo.nmclient, 'client')
+@mock.patch.object(netinfo.nm.device.nmclient, 'client')
 def test_netinfo_show(mock_client, mock_nm):
-    mock_client.return_value.get_devices.return_value = [MockNmDevice()]
+    mock_client.return_value.get_devices.return_value = [
+        MockNmDevice('lo'),
+        MockNmDevice('bond99', type_id=NM_DEVICE_TYPE_BOND)
+    ]
+    mock_nm.DeviceType.BOND = NM_DEVICE_TYPE_BOND
 
     report = netinfo.show()
     iface_names = [iface['name'] for iface in report['interfaces']]
     assert 'lo' in iface_names
+    assert 'bond99' in iface_names
 
 
 class MockNmDevice(object):
+
+    def __init__(self, name, type_id=NM_DEVICE_TYPE_GENERIC):
+        self._name = name
+        self._type_id = type_id
+
     def get_iface(self):
-        return 'lo'
+        return self._name
 
     def get_device_type(self):
-        return NM_DEVICE_TYPE_GENERIC
+        return self._type_id
 
     def get_type_description(self):
         return 'Generic device'
 
     def get_state(self):
         return NM_DEVICE_STATE_UNKNOWN
+
+    def get_active_connection(self):
+        return mock.MagicMock()
