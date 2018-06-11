ac9289491a084b5baa536e7ad068b70b49b0c5e2
Edward Haas <edwardh@redhat.com>
2018-06-11 08:54:50 +0300

    libnmstate: Add bond state validation
    Validate bonds slaves existance and reuse of slaves.

--- libnmstate/validator.py
+++ libnmstate/validator.py
@@ -24,5 +24,35 @@ import jsonschema as js
 from . import schema
 
 
+class LinkAggregationSlavesMissingError(Exception):
+    pass
+
+
+class LinkAggregationSlavesReuseError(Exception):
+    pass
+
+
 def verify(data, validation_schema=schema.ifaces_schema):
     js.validate(data, validation_schema)
+
+
+def verify_interfaces_state(ifaces_state, ifaces_desired_state):
+    verify_link_aggregation_state(ifaces_state, ifaces_desired_state)
+
+
+def verify_link_aggregation_state(ifaces_state, ifaces_desired_state):
+    available_ifaces = {iface_state['name'] for iface_state in ifaces_state}
+    available_ifaces |= {
+        iface_name for iface_name in ifaces_desired_state
+        if ifaces_desired_state[iface_name].get('state') != 'absent'
+    }
+    specified_slaves = set()
+    for iface_state in ifaces_state:
+        link_aggregation = iface_state.get('link-aggregation')
+        if link_aggregation:
+            slaves = set(link_aggregation.get('slaves', []))
+            if not (slaves <= available_ifaces):
+                raise LinkAggregationSlavesMissingError(iface_state)
+            if slaves & specified_slaves:
+                raise LinkAggregationSlavesReuseError(iface_state)
+            specified_slaves |= slaves
