fde5ba0c1707b903f1b0a3c751eab8f87bb27e3f
Edward Haas <edwardh@redhat.com>
2018-06-17 17:47:09 +0300

    libnmstate: Reorganizing nm.translator into classes
    Signed-off-by: Edward Haas <edwardh@redhat.com>

--- libnmstate/nm/translator.py
+++ libnmstate/nm/translator.py
@@ -31,48 +31,47 @@ class ApiIfaceAdminState(object):
     UP = 'up'
 
 
-def nm2api_iface_admin_state(state_name):
-    if state_name == nmclient.NM.DeviceState.ACTIVATED:
-        return ApiIfaceAdminState.UP
-    return ApiIfaceAdminState.DOWN
+class Api2Nm(object):
+    _iface_types_map = None
+
+    @staticmethod
+    def get_iface_type(name):
+        return Api2Nm.get_iface_type_map().get(name, IFACE_TYPE_UNKNOWN)
+
+    @staticmethod
+    def get_iface_type_map():
+        if Api2Nm._iface_types_map is None:
+            Api2Nm._iface_types_map = {
+                'ethernet': nmclient.NM.SETTING_WIRED_SETTING_NAME,
+                'bond': nmclient.NM.SETTING_BOND_SETTING_NAME,
+                'dummy': nmclient.NM.SETTING_DUMMY_SETTING_NAME,
+            }
+        return Api2Nm._iface_types_map
+
+    @staticmethod
+    def get_bond_options(bond_conf):
+        # Is the mode a must config parameter?
+        bond_opts = {'mode': bond_conf['mode']}
+        bond_opts.update(bond_conf.get('options', {}))
+        return bond_opts
 
 
-def api2nm_iface_type(name):
-    return _get_api2nm_iface_type_map().get(name, IFACE_TYPE_UNKNOWN)
+class Nm2Api(object):
+    _iface_types_map = None
 
+    @staticmethod
+    def get_iface_type(name):
+        if Nm2Api._iface_types_map is None:
+            Nm2Api._iface_types_map = Nm2Api._swap_dict_keyval(
+                Api2Nm.get_iface_type_map())
+        return Nm2Api._iface_types_map.get(name, IFACE_TYPE_UNKNOWN)
 
-def nm2api_iface_type(name):
-    return _get_nm2api_iface_type_map().get(name, IFACE_TYPE_UNKNOWN)
+    @staticmethod
+    def get_iface_admin_state(state_name):
+        if state_name == nmclient.NM.DeviceState.ACTIVATED:
+            return ApiIfaceAdminState.UP
+        return ApiIfaceAdminState.DOWN
 
-
-def api2nm_bond_options(bond_conf):
-    # Is the mode a must config parameter?
-    bond_opts = {'mode': bond_conf['mode']}
-    bond_opts.update(bond_conf.get('options', {}))
-    return bond_opts
-
-
-def _swap_dict_keyval(dictionary):
-    return {val: key for key, val in six.viewitems(dictionary)}
-
-
-_api2nm_iface_types = None
-_nm2api_iface_types = None
-
-
-def _get_api2nm_iface_type_map():
-    global _api2nm_iface_types
-    if _api2nm_iface_types is None:
-        _api2nm_iface_types = {
-            'ethernet': nmclient.NM.SETTING_WIRED_SETTING_NAME,
-            'bond': nmclient.NM.SETTING_BOND_SETTING_NAME,
-            'dummy': nmclient.NM.SETTING_DUMMY_SETTING_NAME,
-        }
-    return _api2nm_iface_types
-
-
-def _get_nm2api_iface_type_map():
-    global _nm2api_iface_types
-    if _nm2api_iface_types is None:
-        _nm2api_iface_types = _swap_dict_keyval(_get_api2nm_iface_type_map())
-    return _nm2api_iface_types
+    @staticmethod
+    def _swap_dict_keyval(dictionary):
+        return {val: key for key, val in six.viewitems(dictionary)}
