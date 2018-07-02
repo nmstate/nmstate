ec18dc7c6304daae537887194fb01286f9a2bd6e
Edward Haas <edwardh@redhat.com>
2018-07-02 15:02:40 +0300

    libnmstate: Generalize master/slave metadata handling
    The existing metadata processing handles one specific master type: bond.
    With this change, the processing is generalized to allow other types of
    master/slave interfaces to be supported.

--- libnmstate/netapplier.py
+++ libnmstate/netapplier.py
@@ -70,63 +70,64 @@ def generate_ifaces_metadata(ifaces_desired_state, ifaces_current_state):
     metadata is generated on interfaces, usable by the provider when
     configuring the interface.
     """
-    _generate_link_aggr_metadata(ifaces_desired_state, ifaces_current_state)
+    _generate_link_master_metadata(ifaces_desired_state,
+                                   ifaces_current_state,
+                                   master_type='bond',
+                                   get_slaves_func=_get_bond_slaves_from_state)
 
 
-def _generate_link_aggr_metadata(ifaces_desired_state, ifaces_current_state):
+def _get_bond_slaves_from_state(state, default=()):
+    return state.get('link-aggregation', {}).get('slaves', default)
+
+
+def _generate_link_master_metadata(ifaces_desired_state,
+                                   ifaces_current_state,
+                                   master_type,
+                                   get_slaves_func):
     """
-    Given bonds slaves, add to the slave interface the master information.
+    Given master's slaves, add to the slave interface the master information.
 
     Possible scenarios for a given desired and current sate:
-    - The desired state contains both the bonds and their slaves.
-    - The desired state contains the bonds and partially (or not at all)
+    - The desired state contains both the masters and their slaves.
+    - The desired state contains the masters and partially (or not at all)
       the slaves. Some or all the slaves are in the current state.
-    - Bond is in the current state and some of the slaves are in the desired
+    - Master is in the current state and some of the slaves are in the desired
       state.
     """
-    desired_bonds = [
+    desired_masters = [
         (ifname, ifstate)
         for ifname, ifstate in six.viewitems(ifaces_desired_state)
-        if ifstate['type'] == 'bond'
+        if ifstate['type'] == master_type
     ]
-    for bond_name, bond_state in desired_bonds:
-        desired_link_aggr = bond_state.get('link-aggregation', {})
-        desired_slaves = desired_link_aggr.get('slaves', [])
+    for master_name, master_state in desired_masters:
+        desired_slaves = get_slaves_func(master_state)
         for slave in desired_slaves:
             if slave in ifaces_desired_state:
-                ifaces_desired_state[slave]['_master'] = bond_name
+                ifaces_desired_state[slave]['_master'] = master_name
             elif slave in ifaces_current_state:
-                ifaces_desired_state[slave] = {'_master': bond_name}
+                ifaces_desired_state[slave] = {'_master': master_name}
 
-        desired_slaves = desired_link_aggr.get('slaves')
-        current_bond_state = ifaces_current_state.get(bond_name)
-        if desired_slaves and current_bond_state:
-            current_slaves = current_bond_state['link-aggregation']['slaves']
+        desired_slaves = get_slaves_func(master_state)
+        current_master_state = ifaces_current_state.get(master_name)
+        if desired_slaves and current_master_state:
+            current_slaves = get_slaves_func(current_master_state)
             slaves2remove = (set(current_slaves) - set(desired_slaves))
             for slave in slaves2remove:
                 if slave not in ifaces_desired_state:
                     ifaces_desired_state[slave] = {}
 
-    current_bonds = (
+    current_masters = (
         (ifname, ifstate)
         for ifname, ifstate in six.viewitems(ifaces_current_state)
-        if ifstate['type'] == 'bond'
+        if ifstate['type'] == master_type
     )
-    for bond_name, bond_state in current_bonds:
-        current_link_aggr = bond_state.get('link-aggregation', {})
-        current_slaves = current_link_aggr.get('slaves', [])
+    for master_name, master_state in current_masters:
+        current_slaves = get_slaves_func(master_state)
         for slave in current_slaves:
             if slave in ifaces_desired_state:
-                iface_state = ifaces_desired_state.get(bond_name, {})
-                if not _bond_slaves_defined(iface_state):
-                    ifaces_desired_state[slave]['_master'] = bond_name
-
-
-def _bond_slaves_defined(iface_state):
-    link_aggr = iface_state.get('link-aggregation', {})
-    if 'slaves' in link_aggr:
-        return True
-    return False
+                iface_state = ifaces_desired_state.get(master_name, {})
+                if get_slaves_func(iface_state, None) is None:
+                    ifaces_desired_state[slave]['_master'] = master_name
 
 
 def _add_interfaces(ifaces_desired_state, ifaces_current_state):
