5b3495df8118e948281656abb24f140c2b5e353c
Edward Haas <edwardh@redhat.com>
2018-07-18 14:29:40 +0300

    libnmstate: Use a main-loop for the NM provider
    The NM provider is executing libnm async funtions.
    For proper operation and in order to serialize the actions, GLib
    main-loop use has been embedded through a mainloop object.
    
    The async funtions against libnm are registered to the mainloop object
    and then executed one after the other from that queue.
    The callback functions for each async command are responsible to execute
    the next action in the mainloop object queue.
    
    Signed-off-by: Edward Haas <edwardh@redhat.com>

--- libnmstate/nmclient.py
+++ libnmstate/nmclient.py
@@ -16,16 +16,22 @@
 #
 from __future__ import absolute_import
 
+from collections import deque
+import logging
+
 try:
     import gi                           # pylint: disable=import-error
     gi.require_version('NM', '1.0')     # NOQA: F402
-    from gi.repository import NM        # pylint: disable=import-error
+    from gi.repository import Gio, GLib, NM  # pylint: disable=import-error
 except ImportError:
     # Allow the error to pass in case we are running in a unit test context
     g = None
+    Gio = None
+    GLib = None
     NM = None
 
 
+_mainloop = None
 _nmclient = None
 
 
@@ -36,3 +42,87 @@ def client(refresh=False):
     if _nmclient is None or refresh:
         _nmclient = NM.Client.new(None)
     return _nmclient
+
+
+def mainloop():
+    global _mainloop
+    if _mainloop is None:
+        _mainloop = _MainLoop()
+    return _mainloop
+
+
+class _MainLoop(object):
+    SUCCESS = True
+    FAIL = False
+
+    def __init__(self):
+        self._action_queue = deque()
+        self._mainloop = GLib.MainLoop()
+        self._cancellable = Gio.Cancellable.new()
+
+    def execute_next_action(self):
+        action = self.pop_action()
+        if action:
+            func, args, kwargs = action
+            func(*args, **kwargs)
+        else:
+            logging.debug('NM action queue exhausted, quiting mainloop')
+            self._mainloop.quit()
+
+    def push_action(self, func, *args, **kwargs):
+        action = (func, args, kwargs)
+        self._action_queue.append(action)
+
+    def pop_action(self):
+        try:
+            action = self._action_queue.popleft()
+        except IndexError:
+            return None
+
+        return action if action[0] else None
+
+    def actions_exists(self):
+        return bool(self._action_queue)
+
+    def run(self, timeout):
+        if timeout is None:
+            self._mainloop.run()
+            return _MainLoop.SUCCESS
+
+        timeout_result = []
+        data = (self._mainloop, timeout_result)
+        timeout_id = GLib.timeout_add(
+            int(timeout * 1000),
+            _MainLoop._timeout_cb,
+            data
+        )
+        self._mainloop.run()
+
+        if timeout_result:
+            self._cancellable.cancel()
+            return _MainLoop.FAIL
+
+        GLib.source_remove(timeout_id)
+        return _MainLoop.SUCCESS
+
+    @property
+    def cancellable(self):
+        return self._cancellable
+
+    def quit(self, reason):
+        logging.error('NM main-loop aborted: %s', reason)
+        self._mainloop.quit()
+        self._cancellable.cancel()
+
+    def is_action_canceled(self, err):
+        return (isinstance(err, GLib.GError) and
+                err.domain == 'g-io-error-quark' and
+                err.code == Gio.IOErrorEnum.CANCELLED)
+
+    @staticmethod
+    def _timeout_cb(data):
+        mainloop, result = data
+        result.append(1)
+        logging.warning('NM main-loop timed out.')
+        mainloop.quit()
+        return _MainLoop.FAIL
