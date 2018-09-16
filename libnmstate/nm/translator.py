b1d0f28aa5f86f8bf7ea8096d1042bfaba9c4580
Edward Haas <edwardh@redhat.com>
2018-09-16 21:12:47 +0300

    nm: Reporting should not fail if ovs plugin is missing
    When NetworkManager-ovs package is missing, the report execution failed
    by raising an exception.
    nmstate should not fail in such cases. It is sufficient to ignore OVS
    related interfaces.
    
    Signed-off-by: Edward Haas <edwardh@redhat.com>

--- libnmstate/nm/translator.py
+++ libnmstate/nm/translator.py
@@ -44,11 +44,17 @@ class Api2Nm(object):
                 'ethernet': nmclient.NM.SETTING_WIRED_SETTING_NAME,
                 'bond': nmclient.NM.SETTING_BOND_SETTING_NAME,
                 'dummy': nmclient.NM.SETTING_DUMMY_SETTING_NAME,
-                'ovs-bridge': nmclient.NM.SETTING_OVS_BRIDGE_SETTING_NAME,
-                'ovs-port': nmclient.NM.SETTING_OVS_PORT_SETTING_NAME,
-                'ovs-interface': (
-                    nmclient.NM.SETTING_OVS_INTERFACE_SETTING_NAME),
             }
+            try:
+                Api2Nm._iface_types_map.update({
+                    'ovs-bridge': nmclient.NM.SETTING_OVS_BRIDGE_SETTING_NAME,
+                    'ovs-port': nmclient.NM.SETTING_OVS_PORT_SETTING_NAME,
+                    'ovs-interface': (
+                        nmclient.NM.SETTING_OVS_INTERFACE_SETTING_NAME),
+                })
+            except AttributeError:
+                pass
+
         return Api2Nm._iface_types_map
 
     @staticmethod
