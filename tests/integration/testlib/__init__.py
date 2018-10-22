8046fc3d3ca955c264c3ed2c38c789b339bdd8dd
Edward Haas <edwardh@redhat.com>
2018-10-22 11:26:01 +0300

    tests integ: Use pytest assertions in testlib
    Pytest overwrites the default python assertion with its own expanded
    version which provides extended information in case of failure.
    
    By default, pytest overwirtes the python assertion only in the test
    files. Assert helper packages/modules need to be specified explicitly.
    
    Signed-off-by: Edward Haas <edwardh@redhat.com>

--- tests/integration/testlib/__init__.py
+++ tests/integration/testlib/__init__.py
@@ -14,3 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
+
+import pytest
+
+pytest.register_assert_rewrite(__name__ + '.assertlib')
