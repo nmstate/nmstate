ba82c0a6d4563ab9bec9921e259e0cfcea15b647
Edward Haas <edwardh@redhat.com>
2018-11-05 08:59:45 +0200

    tests integ: Add and remove a bond with two slaves
    Test a basic bond setup by creating a new bond over two slaves.
    The test also covers bond removal as part of the cleanup stage.
    
    Signed-off-by: Edward Haas <edwardh@redhat.com>

---
+++ tests/integration/bond_test.py
@@ -0,0 +1,53 @@
+#
+# Copyright 2018 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+import copy
+
+import yaml
+
+from libnmstate import netapplier
+
+from .testlib import assertlib
+from .testlib import statelib
+from .testlib.statelib import INTERFACES
+
+
+BOND99_YAML_BASE = """
+interfaces:
+- name: bond99
+  type: bond
+  state: up
+  link-aggregation:
+    mode: balance-rr
+    slaves:
+    - eth1
+    - eth2
+"""
+
+
+def test_add_and_remove_bond_with_two_slaves():
+    state = yaml.load(BOND99_YAML_BASE)
+    netapplier.apply(copy.deepcopy(state))
+
+    assertlib.assert_state(state)
+
+    state[INTERFACES][0]['state'] = 'absent'
+
+    netapplier.apply(copy.deepcopy(state))
+
+    state = statelib.show_only((state[INTERFACES][0]['name'],))
+    assert not state[INTERFACES]
