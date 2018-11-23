6aaf17b1ddce29404325847f955e3ba95ed5226c
Edward Haas <edwardh@redhat.com>
2018-11-23 12:33:47 +0200

    debug: FOR DEBUG ONLY - Log nmcli ouput.
    Signed-off-by: Edward Haas <edwardh@redhat.com>

--- tests/integration/nm/vlan_test.py
+++ tests/integration/nm/vlan_test.py
@@ -16,10 +16,12 @@
 #
 
 from contextlib import contextmanager
-import time
+# import logging
+# import time
 
 from libnmstate import nm
 
+# from ..testlib import cmd
 from .testlib import mainloop
 
 
@@ -34,8 +36,12 @@ def test_create_and_remove_vlan():
         vlan_current_state = _get_vlan_current_state(vlan_desired_state)
         assert vlan_desired_state == vlan_current_state
 
-    # FIXME: Remove this sleep after adding wait for state mechanism.
-    time.sleep(5)
+    # # FIXME: Remove this sleep after adding wait for state mechanism.
+    # time.sleep(5)
+    # _, devout, _ = cmd.exec_cmd(['nmcli', 'dev'])
+    # _, conout, _ = cmd.exec_cmd(['nmcli', 'con'])
+    # logging.debug('### nmcli dev: %s', devout)
+    # logging.debug('### nmcli con: %s', conout)
 
     assert not _get_vlan_current_state(vlan_desired_state)
 
@@ -50,6 +56,7 @@ def _vlan_interface(state):
 
 
 def _get_vlan_current_state(vlan_desired_state):
+    nm.nmclient.client(refresh=True)
     ifname = _get_vlan_ifname(vlan_desired_state)
     nmdev = nm.device.get_device_by_name(ifname)
     return nm.vlan.get_info(nmdev) if nmdev else {}
