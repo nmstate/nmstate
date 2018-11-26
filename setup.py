000abc9723074bdf4924382900dbf3722c2f579f
Gris Ge <fge@redhat.com>
2018-11-26 23:40:11 +0800

    Remove requirement of pbr.
     * Use setuptools instead in order to support RHEL 8 beta where pbr is
       not included.
     * The MANIFEST.in file is for including 'requirements.txt' in
       output of `python setup.py sdisk` command.
    
    Signed-off-by: Gris Ge <fge@redhat.com>

--- setup.py
+++ setup.py
@@ -1,3 +1,36 @@
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup(setup_requires=['pbr'], pbr=True)
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+def requirements():
+    req = []
+    with open('requirements.txt') as f:
+        for l in f:
+            l.strip()
+            if not l.startswith('#'):
+                req.append(l)
+    return req
+
+
+setup(
+    name='nmstate',
+    version='0.0.2',
+    description='Declarative network manager API',
+    author="Edward Haas",
+    author_email="ehaas@redhat.com",
+    long_description=readme(),
+    url='https://nmstate.github.io/',
+    license='GPLv2+',
+    packages=find_packages(),
+    install_requires=requirements(),
+    entry_points={
+        'console_scripts': ['nmstatectl = nmstatectl.nmstatectl:main'],
+    },
+    package_data={
+        'libnmstate': ['schemas/operational-state.yaml']
+    },
+)
