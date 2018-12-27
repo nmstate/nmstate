d554ba9bbe0a532dfee75b286e150b30a8b7abb2
Edward Haas <edwardh@redhat.com>
2018-12-27 15:52:16 +0200

    nm: Reduce noise when logging actions execution
    The logging of the action function arguments is not helpful, makes it
    difficult to follow the logs, therefore, they are removed.
    
    Signed-off-by: Edward Haas <edwardh@redhat.com>

--- libnmstate/nm/nmclient.py
+++ libnmstate/nm/nmclient.py
@@ -73,8 +73,7 @@ class _MainLoop(object):
         action = self.pop_action()
         if action:
             func, args, kwargs = action
-            logging.debug('Executing NM action: func=%s, args=%s',
-                          func.__name__, args)
+            logging.debug('Executing NM action: func=%s', func.__name__)
             func(*args, **kwargs)
         else:
             logging.debug('NM action queue exhausted, quiting mainloop')
