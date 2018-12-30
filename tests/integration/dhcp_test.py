ca344a38134c559c1e99ea6b18e9ece57e6e343f
Sveta Haas <sveta.haas@gmail.com>
2018-12-30 16:03:39 +0200

    integ tests: Add bond with dhcp and slave
    Signed-off-by: Sveta Haas <sveta.haas@gmail.com>

--- tests/integration/dhcp_test.py
+++ tests/integration/dhcp_test.py
@@ -68,6 +68,21 @@ def dhcp_env():
         _clean_up()
 
 
+@pytest.fixture
+def setup_remove_bond99():
+    yield
+    remove_bond = {
+        INTERFACES: [
+            {
+                'name': 'bond99',
+                'type': 'bond',
+                'state': 'absent'
+            }
+        ]
+    }
+    netapplier.apply(remove_bond)
+
+
 def test_ipv4_dhcp(dhcp_env):
     desired_state = statelib.show_only((DHCP_CLI_NIC,))
     dhcp_cli_desired_state = desired_state[INTERFACES][0]
@@ -138,6 +153,35 @@ def test_dhcp_with_addresses(dhcp_env):
                         {'ip': IPV6_ADDRESS1, 'prefix-length': 64},
                         {'ip': IPV6_ADDRESS2, 'prefix-length': 64}
                     ]
+                }
+            }
+        ]
+    }
+
+    netapplier.apply(desired_state)
+
+    assertlib.assert_state(desired_state)
+
+
+def test_dhcp_for_bond_with_ip_address_and_slave(dhcp_env,
+                                                 setup_remove_bond99):
+
+    desired_state = {
+        INTERFACES: [
+            {
+                'name': 'bond99',
+                'type': 'bond',
+                'state': 'up',
+                'ipv4': {
+                    'enabled': True,
+                    'dhcp': True
+                },
+
+                'link-aggregation': {
+                    'mode': 'balance-rr',
+                    'slaves': [DHCP_CLI_NIC],
+                    'options':
+                        {'miimon': '140'}
                 }
             }
         ]
