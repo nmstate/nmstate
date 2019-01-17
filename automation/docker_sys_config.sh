e8bec7c31438b2c2ca16eedb426de2ce9cc8540f
Gris Ge <fge@redhat.com>
2019-01-17 20:03:54 +0800

    Add Fedora CI test docker image.
    Add `Dockerfile.fedora` file to Fedora CI test docker image.
    The common shared scripts between Centos and Fedora docker image are
    moved to standalone script: `docker_enable_systemd.sh` and
    `docker_sys_config.sh`.
    
    Signed-off-by: Gris Ge <fge@redhat.com>

---
+++ automation/docker_sys_config.sh
@@ -0,0 +1,10 @@
+install -o root -g root -d /etc/sysconfig/network-scripts
+echo -e "[logging]\nlevel=TRACE\ndomains=ALL\n" > \
+    /etc/NetworkManager/conf.d/97-docker-build.conf
+echo -e "[device]\nmatch-device=*\nmanaged=0\n" >> \
+    /etc/NetworkManager/conf.d/97-docker-build.conf
+sed -i 's/#RateLimitInterval=30s/RateLimitInterval=0/' \
+    /etc/systemd/journald.conf
+sed -i 's/#RateLimitBurst=1000/RateLimitBurst=0/' \
+    /etc/systemd/journald.conf
+systemctl enable openvswitch.service
