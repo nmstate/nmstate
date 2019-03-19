33d6e1baea5dacf3f24f9565a0e18d7725c069ca
Edward Haas <edwardh@redhat.com>
2019-03-19 17:22:19 +0200

    netapplier: Relocate iface state assert to the state module
    Signed-off-by: Edward Haas <edwardh@redhat.com>

---
+++ tests/lib/state_test.py
@@ -0,0 +1,135 @@
+#
+# Copyright 2019 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+import pytest
+
+from libnmstate import state
+from libnmstate.error import NmstateVerificationError
+from libnmstate.schema import Interface
+
+
+class TestAssertIfaceState(object):
+
+    def test_desired_is_identical_to_current(self):
+        desired_state = self._base_state
+        current_state = self._base_state
+
+        desired_state.verify_interfaces(current_state)
+
+    def test_desired_is_partial_to_current(self):
+        desired_state = self._base_state
+        current_state = self._base_state
+        current_state.interfaces.update({
+            'eth0': {'name': 'eth0', 'state': 'up', 'type': 'unknown'},
+            'eth1': {'name': 'eth1', 'state': 'up', 'type': 'unknown'}
+        })
+
+        desired_state.verify_interfaces(current_state)
+
+    def test_current_is_partial_to_desired(self):
+        desired_state = self._base_state
+        current_state = self._base_state
+        desired_state.interfaces.update({
+            'eth0': {'name': 'eth0', 'state': 'up', 'type': 'unknown'},
+            'eth1': {'name': 'eth1', 'state': 'up', 'type': 'unknown'}
+        })
+
+        with pytest.raises(NmstateVerificationError):
+            desired_state.verify_interfaces(current_state)
+
+    def test_desired_is_not_equal_to_current(self):
+        desired_state = self._base_state
+        current_state = self._base_state
+        current_state.interfaces['foo-name']['state'] = 'down'
+
+        with pytest.raises(NmstateVerificationError):
+            desired_state.verify_interfaces(current_state)
+
+    def test_sort_multiple_ip(self):
+        desired_state = self._base_state
+        current_state = self._base_state
+        desired_state.interfaces['foo-name']['ipv4'] = {
+            'address': [
+                {
+                    'ip': '192.168.122.10',
+                    'prefix-length': 24
+                },
+                {
+                    'ip': '192.168.121.10',
+                    'prefix-length': 24
+                },
+            ],
+            'enabled': True
+        }
+        current_state.interfaces['foo-name']['ipv4'] = {
+            'address': [
+                {
+                    'ip': '192.168.121.10',
+                    'prefix-length': 24
+                },
+                {
+                    'ip': '192.168.122.10',
+                    'prefix-length': 24
+                },
+            ],
+            'enabled': True
+        }
+        desired_state.interfaces['foo-name']['ipv6'] = {
+            'address': [
+                {
+                    'ip': '2001::2',
+                    'prefix-length': 64
+                },
+                {
+                    'ip': '2001::1',
+                    'prefix-length': 64
+                }
+            ],
+            'enabled': True
+        }
+        current_state.interfaces['foo-name']['ipv6'] = {
+            'address': [
+                {
+                    'ip': '2001::1',
+                    'prefix-length': 64
+                },
+                {
+                    'ip': '2001::2',
+                    'prefix-length': 64
+                }
+            ],
+            'enabled': True
+        }
+
+        desired_state.verify_interfaces(current_state)
+
+    @property
+    def _base_state(self):
+        return state.State({
+            Interface.KEY: [
+                {
+                    'name': 'foo-name',
+                    'type': 'foo-type',
+                    'state': 'up',
+                    'bridge': {
+                        'port': [
+                            {'name': 'eth0', 'type': 'system'}
+                        ]
+                    }
+                }
+            ]
+        })
