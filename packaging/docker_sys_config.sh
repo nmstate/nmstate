70047bfa132acd10b6515a060375a2375c1e2377
Till Maas <opensource@till.name>
2019-04-26 20:19:05 +0200

    container: Reorganize system configuration
    Split NetworkManager configuration into individual files to simplify
    disabling workarounds from container images. Also improve readability
    and group related changes.
    
    Signed-off-by: Till Maas <opensource@till.name>

--- packaging/docker_sys_config.sh
+++ packaging/docker_sys_config.sh
@@ -1,12 +1,26 @@
 install -o root -g root -d /etc/sysconfig/network-scripts
-echo -e "[logging]\nlevel=TRACE\ndomains=ALL\n" > \
-    /etc/NetworkManager/conf.d/97-docker-build.conf
-echo -e "[device]\nmatch-device=*\nmanaged=0\n" >> \
-    /etc/NetworkManager/conf.d/97-docker-build.conf
-echo -e "[main]\nno-auto-default=*\n" >> \
-    /etc/NetworkManager/conf.d/97-docker-build.conf
-sed -i 's/#RateLimitInterval=30s/RateLimitInterval=0/' \
+
+cat > /etc/NetworkManager/conf.d/97-trace-logging.conf <<EOF
+[logging]
+level=TRACE
+domains=ALL
+EOF
+
+sed -i \
+    -e 's/^#RateLimitInterval=.*/RateLimitInterval=0/' \
+    -e 's/^#RateLimitBurst=.*/RateLimitBurst=0/' \
     /etc/systemd/journald.conf
-sed -i 's/#RateLimitBurst=1000/RateLimitBurst=0/' \
-    /etc/systemd/journald.conf
+
+cat > /etc/NetworkManager/conf.d/97-no-managed.conf <<EOF
+[device]
+match-device=*
+managed=0
+EOF
+
+cat > /etc/NetworkManager/conf.d/97-no-auto-default.conf <<EOF
+# Workaround for https://bugzilla.redhat.com/1687937
+[main]
+no-auto-default=*
+EOF
+
 systemctl enable openvswitch.service
