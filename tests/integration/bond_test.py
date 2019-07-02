ab62eeadbe223a2fdd2bad3aa345180460e3162a
Gris Ge <fge@redhat.com>
2019-07-02 23:42:55 +0800

    Relicense to LGPL2.1+
    Signed-off-by: Gris Ge <fge@redhat.com>

--- tests/integration/bond_test.py
+++ tests/integration/bond_test.py
@@ -1,18 +1,20 @@
 #
-# Copyright 2018 Red Hat, Inc.
+# Copyright (c) 2018-2019 Red Hat, Inc.
+#
+# This file is part of nmstate
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 2.1 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 #
 
 from contextlib import contextmanager
