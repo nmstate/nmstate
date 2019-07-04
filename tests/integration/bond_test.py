3846c8ff7e09079b06df067cbda7eac487caf7ab
Edward Haas <edwardh@redhat.com>
2019-07-04 22:23:00 +0300

    tests integ: Refactor bond test module
    Signed-off-by: Edward Haas <edwardh@redhat.com>

--- tests/integration/bond_test.py
+++ tests/integration/bond_test.py
@@ -24,12 +24,18 @@ import yaml
 
 import libnmstate
 from libnmstate.error import NmstateVerificationError
+from libnmstate.schema import Bond
+from libnmstate.schema import BondMode
 from libnmstate.schema import Interface
+from libnmstate.schema import InterfaceState
+from libnmstate.schema import InterfaceType
 
 from .testlib import assertlib
 from .testlib import statelib
-from .testlib.statelib import INTERFACES
 from .testlib.assertlib import assert_mac_address
+
+
+BOND99 = 'bond99'
 
 MAC0 = '02:ff:ff:ff:ff:00'
 MAC1 = '02:ff:ff:ff:ff:01'
@@ -51,20 +57,39 @@ interfaces:
 def setup_remove_bond99():
     yield
     remove_bond = {
-        INTERFACES: [{'name': 'bond99', 'type': 'bond', 'state': 'absent'}]
+        Interface.KEY: [
+            {
+                Interface.NAME: 'bond99',
+                Interface.TYPE: InterfaceType.BOND,
+                Interface.STATE: InterfaceState.ABSENT,
+            }
+        ]
     }
     libnmstate.apply(remove_bond)
+
+
+@pytest.fixture
+def bond99_with_2_slaves(eth1_up, eth2_up):
+    slaves = [
+        eth1_up[Interface.KEY][0][Interface.NAME],
+        eth2_up[Interface.KEY][0][Interface.NAME],
+    ]
+    with bond_interface('bond99', slaves) as state:
+        yield state
 
 
 @contextmanager
 def bond_interface(name, slaves):
     desired_state = {
-        INTERFACES: [
+        Interface.KEY: [
             {
-                'name': name,
-                'type': 'bond',
-                'state': 'up',
-                'link-aggregation': {'mode': 'balance-rr', 'slaves': slaves},
+                Interface.NAME: name,
+                Interface.TYPE: InterfaceType.BOND,
+                Interface.STATE: InterfaceState.UP,
+                Bond.CONFIG_SUBTREE: {
+                    Bond.MODE: BondMode.ROUND_ROBIN,
+                    Bond.SLAVES: slaves,
+                },
             }
         ]
     }
@@ -73,7 +98,15 @@ def bond_interface(name, slaves):
         yield desired_state
     finally:
         libnmstate.apply(
-            {INTERFACES: [{'name': name, 'type': 'bond', 'state': 'absent'}]}
+            {
+                Interface.KEY: [
+                    {
+                        Interface.NAME: name,
+                        Interface.TYPE: InterfaceType.BOND,
+                        Interface.STATE: InterfaceState.ABSENT,
+                    }
+                ]
+            }
         )
 
 
@@ -83,49 +116,60 @@ def test_add_and_remove_bond_with_two_slaves(eth1_up, eth2_up):
 
     assertlib.assert_state(state)
 
-    state[INTERFACES][0]['state'] = 'absent'
+    state[Interface.KEY][0][Interface.STATE] = InterfaceState.ABSENT
 
     libnmstate.apply(state)
 
-    state = statelib.show_only((state[INTERFACES][0]['name'],))
-    assert not state[INTERFACES]
+    state = statelib.show_only((state[Interface.KEY][0][Interface.NAME],))
+    assert not state[Interface.KEY]
 
 
 def test_remove_bond_with_minimum_desired_state(eth1_up, eth2_up):
     state = yaml.load(BOND99_YAML_BASE, Loader=yaml.SafeLoader)
+    bond_name = state[Interface.KEY][0][Interface.NAME]
+
     libnmstate.apply(state)
 
     remove_bond_state = {
-        INTERFACES: [{'name': 'bond99', 'type': 'bond', 'state': 'absent'}]
+        Interface.KEY: [
+            {
+                Interface.NAME: bond_name,
+                Interface.TYPE: InterfaceType.BOND,
+                Interface.STATE: InterfaceState.ABSENT,
+            }
+        ]
     }
     libnmstate.apply(remove_bond_state)
-    state = statelib.show_only((state[INTERFACES][0]['name'],))
-    assert not state[INTERFACES]
+    state = statelib.show_only((bond_name,))
+    assert not state[Interface.KEY]
 
 
 def test_add_bond_without_slaves():
-    with bond_interface(name='bond99', slaves=[]) as bond_state:
+    with bond_interface(name='bond99', slaves=[]) as state:
 
-        assert bond_state[INTERFACES][0]['link-aggregation']['slaves'] == []
+        assert state[Interface.KEY][0][Bond.CONFIG_SUBTREE][Bond.SLAVES] == []
 
 
 def test_add_bond_with_slaves_and_ipv4(eth1_up, eth2_up, setup_remove_bond99):
     desired_bond_state = {
-        INTERFACES: [
+        Interface.KEY: [
             {
-                'name': 'bond99',
-                'type': 'bond',
-                'state': 'up',
-                'ipv4': {
+                Interface.NAME: 'bond99',
+                Interface.TYPE: InterfaceType.BOND,
+                Interface.STATE: InterfaceState.UP,
+                Interface.IPV4: {
                     'enabled': True,
                     'address': [
                         {'ip': '192.168.122.250', 'prefix-length': 24}
                     ],
                 },
-                'link-aggregation': {
-                    'mode': 'balance-rr',
-                    'slaves': ['eth1', 'eth2'],
-                    'options': {'miimon': '140'},
+                Bond.CONFIG_SUBTREE: {
+                    Bond.MODE: BondMode.ROUND_ROBIN,
+                    Bond.SLAVES: [
+                        eth1_up[Interface.KEY][0][Interface.NAME],
+                        eth2_up[Interface.KEY][0][Interface.NAME],
+                    ],
+                    Bond.OPTIONS_SUBTREE: {'miimon': '140'},
                 },
             }
         ]
@@ -139,27 +183,30 @@ def test_add_bond_with_slaves_and_ipv4(eth1_up, eth2_up, setup_remove_bond99):
 def test_rollback_for_bond(eth1_up, eth2_up):
     current_state = libnmstate.show()
     desired_state = {
-        INTERFACES: [
+        Interface.KEY: [
             {
-                'name': 'bond99',
-                'type': 'bond',
-                'state': 'up',
-                'ipv4': {
+                Interface.NAME: 'bond99',
+                Interface.TYPE: InterfaceType.BOND,
+                Interface.STATE: InterfaceState.UP,
+                Interface.IPV4: {
                     'enabled': True,
                     'address': [
                         {'ip': '192.168.122.250', 'prefix-length': 24}
                     ],
                 },
-                'link-aggregation': {
-                    'mode': 'balance-rr',
-                    'slaves': ['eth1', 'eth2'],
-                    'options': {'miimon': '140'},
+                Bond.CONFIG_SUBTREE: {
+                    Bond.MODE: BondMode.ROUND_ROBIN,
+                    Bond.SLAVES: [
+                        eth1_up[Interface.KEY][0][Interface.NAME],
+                        eth2_up[Interface.KEY][0][Interface.NAME],
+                    ],
+                    Bond.OPTIONS_SUBTREE: {'miimon': '140'},
                 },
             }
         ]
     }
 
-    desired_state[INTERFACES][0]['invalid_key'] = 'foo'
+    desired_state[Interface.KEY][0]['invalid_key'] = 'foo'
 
     with pytest.raises(NmstateVerificationError):
         libnmstate.apply(desired_state)
@@ -167,91 +214,101 @@ def test_rollback_for_bond(eth1_up, eth2_up):
     time.sleep(5)
 
     current_state_after_apply = libnmstate.show()
-    assert current_state[INTERFACES] == current_state_after_apply[INTERFACES]
+    assert (
+        current_state[Interface.KEY]
+        == current_state_after_apply[Interface.KEY]
+    )
 
 
 def test_add_slave_to_bond_without_slaves(eth1_up):
+    slave_name = eth1_up[Interface.KEY][0][Interface.NAME]
+    with bond_interface(name=BOND99, slaves=[]) as state:
+        bond_state = state[Interface.KEY][0]
+        bond_state[Bond.CONFIG_SUBTREE][Bond.SLAVES] = [slave_name]
+        libnmstate.apply(state)
 
-    with bond_interface(name='bond99', slaves=[]) as bond_state:
+        current_state = statelib.show_only((BOND99,))
+        bond_cur_state = current_state[Interface.KEY][0]
 
-        bond_state[INTERFACES][0]['link-aggregation']['slaves'] = ['eth1']
-        libnmstate.apply(bond_state)
-
-        current_state = statelib.show_only(('bond99',))
-        bond99_cur_state = current_state[INTERFACES][0]
-
-        assert bond99_cur_state['link-aggregation']['slaves'][0] == 'eth1'
+        assert bond_cur_state[Bond.CONFIG_SUBTREE][Bond.SLAVES] == [slave_name]
 
 
 @pytest.mark.xfail(strict=True, reason="Jira issue # NMSTATE-143")
 def test_remove_all_slaves_from_bond(eth1_up):
+    slave_name = (eth1_up[Interface.KEY][0][Interface.NAME],)
+    with bond_interface(name=BOND99, slaves=[slave_name]) as state:
+        state[Interface.KEY][0][Bond.CONFIG_SUBTREE][Bond.SLAVES] = []
 
-    with bond_interface(name='bond99', slaves=['eth1']) as bond_state:
-        bond_state[INTERFACES][0]['link-aggregation']['slaves'] = []
+        libnmstate.apply(state)
 
-        libnmstate.apply(bond_state)
+        current_state = statelib.show_only((BOND99,))
+        bond_cur_state = current_state[Interface.KEY][0]
 
-        current_state = statelib.show_only(('bond99',))
-        bond99_cur_state = current_state[INTERFACES][0]
-
-        assert bond99_cur_state['link-aggregation']['slaves'] == []
+        assert bond_cur_state[Bond.CONFIG_SUBTREE][Bond.SLAVES] == []
 
 
 def test_replace_bond_slave(eth1_up, eth2_up):
+    slave1_name = eth1_up[Interface.KEY][0][Interface.NAME]
+    slave2_name = eth2_up[Interface.KEY][0][Interface.NAME]
+    with bond_interface(name=BOND99, slaves=[slave1_name]) as state:
+        bond_state = state[Interface.KEY][0]
+        bond_state[Bond.CONFIG_SUBTREE][Bond.SLAVES] = [slave2_name]
 
-    with bond_interface(name='bond99', slaves=['eth1']) as bond_state:
-        bond_state[INTERFACES][0]['link-aggregation']['slaves'] = ['eth2']
+        libnmstate.apply(state)
 
-        libnmstate.apply(bond_state)
+        current_state = statelib.show_only((BOND99,))
+        bond_cur_state = current_state[Interface.KEY][0]
 
-        current_state = statelib.show_only(('bond99',))
-        bond99_cur_state = current_state[INTERFACES][0]
-
-        assert bond99_cur_state['link-aggregation']['slaves'][0] == 'eth2'
+        assert bond_cur_state[Bond.CONFIG_SUBTREE][Bond.SLAVES] == [
+            slave2_name
+        ]
 
 
 def test_remove_one_of_the_bond_slaves(eth1_up, eth2_up):
-    with bond_interface(name='bond99', slaves=['eth1', 'eth2']) as bond_state:
+    slave1_name = eth1_up[Interface.KEY][0][Interface.NAME]
+    slave2_name = eth2_up[Interface.KEY][0][Interface.NAME]
+    with bond_interface(
+        name=BOND99, slaves=[slave1_name, slave2_name]
+    ) as state:
+        bond_state = state[Interface.KEY][0]
+        bond_state[Bond.CONFIG_SUBTREE][Bond.SLAVES] = [slave2_name]
 
-        bond_state[INTERFACES][0]['link-aggregation']['slaves'] = ['eth2']
+        libnmstate.apply(state)
 
-        libnmstate.apply(bond_state)
+        current_state = statelib.show_only((BOND99,))
+        bond_cur_state = current_state[Interface.KEY][0]
 
-        current_state = statelib.show_only(('bond99',))
-        bond99_cur_state = current_state[INTERFACES][0]
-
-    assert bond99_cur_state['link-aggregation']['slaves'] == ['eth2']
+    assert bond_cur_state[Bond.CONFIG_SUBTREE][Bond.SLAVES] == [slave2_name]
 
 
 def test_set_bond_mac_address(eth1_up):
-    with bond_interface(name='bond99', slaves=['eth1']) as bond_state:
-        bond_state[Interface.KEY][0][Interface.MAC] = MAC0
-        libnmstate.apply(bond_state)
+    slave_name = eth1_up[Interface.KEY][0][Interface.NAME]
+    with bond_interface(name=BOND99, slaves=[slave_name]) as state:
+        state[Interface.KEY][0][Interface.MAC] = MAC0
+        libnmstate.apply(state)
 
-        current_state = statelib.show_only(('bond99', 'eth1'))
+        current_state = statelib.show_only((BOND99, slave_name))
         assert_mac_address(current_state, MAC0)
 
-        bond_state[Interface.KEY][0][Interface.MAC] = MAC1
-        libnmstate.apply(bond_state)
+        state[Interface.KEY][0][Interface.MAC] = MAC1
+        libnmstate.apply(state)
 
-        current_state = statelib.show_only(('bond99', 'eth1'))
+        current_state = statelib.show_only((BOND99, slave_name))
         assert_mac_address(current_state, MAC1)
 
 
-@pytest.fixture
-def bond99():
-    with bond_interface(name='bond99', slaves=['eth1', 'eth2']) as bond_state:
-        yield bond_state
+def test_reordering_the_slaves_does_not_change_the_mac(bond99_with_2_slaves):
+    bond_state = bond99_with_2_slaves[Interface.KEY][0]
+    bond_slaves = bond_state[Bond.CONFIG_SUBTREE][Bond.SLAVES]
+    ifaces_names = [bond_state[Interface.NAME]] + bond_slaves
 
-
-def test_reordering_the_slaves_does_not_change_the_mac(bond99):
-    current_state = statelib.show_only(('bond99', 'eth1', 'eth2'))
+    current_state = statelib.show_only(ifaces_names)
     assert_mac_address(current_state)
 
-    bond99[INTERFACES][0]['link-aggregation']['slaves'].reverse()
-    libnmstate.apply(bond99)
+    bond_slaves.reverse()
+    libnmstate.apply(bond99_with_2_slaves)
 
-    modified_state = statelib.show_only(('bond99', 'eth1', 'eth2'))
+    modified_state = statelib.show_only(ifaces_names)
     assert_mac_address(
         modified_state, current_state[Interface.KEY][0][Interface.MAC]
     )
