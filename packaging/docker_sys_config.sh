89597cb660dae6818858663a3890ec1155862d2e
Gris Ge <fge@redhat.com>
2019-11-11 23:28:08 +0800

    test container: Drop the no auto managed device config
    There is no need to instruct NetworkManager to mark
    all devices as unmanaged any more.
    
    Signed-off-by: Gris Ge <fge@redhat.com>

--- packaging/docker_sys_config.sh
+++ packaging/docker_sys_config.sh
@@ -11,12 +11,6 @@ sed -i \
     -e 's/^#RateLimitBurst=.*/RateLimitBurst=0/' \
     /etc/systemd/journald.conf
 
-cat > /etc/NetworkManager/conf.d/97-no-managed.conf <<EOF
-[device]
-match-device=*
-managed=0
-EOF
-
 cat > /etc/NetworkManager/conf.d/97-no-auto-default.conf <<EOF
 # Workaround for https://bugzilla.redhat.com/1687937
 [main]
