ed9360cabc8880d05d257bf9446e560145b58684
Gris Ge <fge@redhat.com>
2019-11-11 23:29:59 +0800

    test container: Drop the NM no auto default config
    The NetworkManager-config-server rpm packages now contains
    NetworkManager config for disabling auto created profile for
    all devices, there is not need to do it manually.
    
    Signed-off-by: Gris Ge <fge@redhat.com>

--- packaging/docker_sys_config.sh
+++ packaging/docker_sys_config.sh
@@ -11,12 +11,6 @@ sed -i \
     -e 's/^#RateLimitBurst=.*/RateLimitBurst=0/' \
     /etc/systemd/journald.conf
 
-cat > /etc/NetworkManager/conf.d/97-no-auto-default.conf <<EOF
-# Workaround for https://bugzilla.redhat.com/1687937
-[main]
-no-auto-default=*
-EOF
-
 cat > /etc/NetworkManager/conf.d/97-use-dhclient.conf <<EOF
 # Workaround for https://bugzilla.redhat.com/show_bug.cgi?id=1749358
 [main]
