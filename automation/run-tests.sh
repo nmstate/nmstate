63d7f8f75d8155c90e7491e77e17dc4c25a73812
Till Maas <opensource@till.name>
2019-11-12 21:24:33 +0100

    automation: Create Veth interfaces in container
    Create the test veth interfaces inside the container to allow to run
    multiple containers at once. Another benefit is that this simplifies
    to use podman instead of docker.
    
    Signed-off-by: Till Maas <opensource@till.name>

--- automation/run-tests.sh
+++ automation/run-tests.sh
@@ -7,9 +7,6 @@ CONT_EXPORT_DIR="/exported-artifacts"
 
 CONTAINER_WORKSPACE="/workspace/nmstate"
 
-NET0="nmstate-net0"
-NET1="nmstate-net1"
-
 TEST_TYPE_ALL="all"
 TEST_TYPE_FORMAT="format"
 TEST_TYPE_LINT="lint"
@@ -28,8 +25,6 @@ function remove_container {
     [ "$res" -ne 0 ] && echo "*** ERROR: $res"
     docker_exec 'rm -rf $CONTAINER_WORKSPACE/*nmstate*.rpm'
     docker rm $CONTAINER_ID -f
-    docker network rm $NET0
-    docker network rm $NET1
 }
 
 function pyclean {
@@ -43,11 +38,9 @@ function docker_exec {
 }
 
 function add_extra_networks {
-    docker network create $NET0 || true
-    docker network create $NET1 || true
-    docker network connect $NET0 $CONTAINER_ID
-    docker network connect $NET1 $CONTAINER_ID
     docker_exec '
+      ip link add eth1 type veth peer eth1peer && \
+      ip link add eth2 type veth peer eth2peer && \
       ip addr flush eth1 && \
       ip addr flush eth2
     '
