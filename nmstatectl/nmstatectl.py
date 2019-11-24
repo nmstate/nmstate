7469aa25a235ea36794d6a8341433bc5174c6f52
Edward Haas <edwardh@redhat.com>
2019-11-24 21:02:13 +0200

    python3: Remove future statements
    Now that Nmstate is no longer supporting python2, the future statements
    are removed.
    
    Signed-off-by: Edward Haas <edwardh@redhat.com>

--- nmstatectl/nmstatectl.py
+++ nmstatectl/nmstatectl.py
@@ -16,8 +16,6 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 #
-from __future__ import absolute_import
-
 import argparse
 import errno
 import fnmatch
