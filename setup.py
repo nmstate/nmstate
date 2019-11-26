cf941b91c723f9405bd7ce4329d991af43a5036b
Till Maas <opensource@till.name>
2019-11-26 20:16:55 +0100

    setup: Remove workaround for old setuptools
    Nmstate does not need to support old setuptools anymore.
    This reverts commit 2ef3672133ef3abf39866e871dcda0d0a84eb768.

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-import logging
-import setuptools
 from setuptools import setup, find_packages
 from datetime import date
 
@@ -9,31 +7,11 @@ def readme():
         return f.read()
 
 
-def _is_outdated_setuptools():
-    """
-    Return True if setuptool is older than 20.5 which introduced environment
-    markers for requirements:
-        https://www.python.org/dev/peps/pep-0508/#environment-markers
-    """
-    major, minor = setuptools.__version__.split('.')[:2]
-    return (int(major), int(minor)) < (20, 5)
-
-
 def requirements():
     req = []
-    setuptools_is_outdated = _is_outdated_setuptools()
     with open('requirements.txt') as fd:
         for line in fd:
             line.strip()
-            if setuptools_is_outdated:
-                if ';' in line:
-                    # Remove the environment marker
-                    logging.warning(
-                        "Your setuptools is too old(<20.5) to support "
-                        "environment marker, removing the environment marker "
-                        "as workaround but it could be buggy"
-                    )
-                    line = line.split(';')[0]
             if not line.startswith('#'):
                 req.append(line)
     return req
