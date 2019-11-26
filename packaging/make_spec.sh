e90fd3e07c482ec8e2038c6c65b86809ffaba6ce
Till Maas <opensource@till.name>
2019-11-26 21:37:00 +0100

    packaging/spec: Remove Python 3 suffix
    Signed-off-by: Till Maas <opensource@till.name>

--- packaging/make_spec.sh
+++ packaging/make_spec.sh
@@ -8,16 +8,7 @@ eval "$(${SRC_DIR}/packaging/get_version.sh)"
 
 # spec variable is used by COPR as well: https://docs.pagure.org/copr.copr/user_documentation.html
 if [ -z "${spec}" ]; then
-    if [ -n "$(rpm -E %{?fedora} 2>/dev/null)" ] ||
-       [ -n "$(rpm -E %{?el8} 2>/dev/null)" ]; then
-        pysuffix=.py3
-    elif [ -n "$(rpm -E %{?el7} 2>/dev/null)" ]; then
-        pysuffix=.py2
-    else
-        echo "Not supported" >&2
-        exit 1
-    fi
-    spec="${SRC_DIR}/packaging/nmstate${pysuffix}.spec"
+    spec="${SRC_DIR}/packaging/nmstate.spec"
 fi
 
 
