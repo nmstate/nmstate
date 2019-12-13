66afd9f02e90536e7ad701d9d7bdf595b0bc4fcf
Fernando Fernandez Mancera <ffmancera@riseup.net>
2019-12-13 18:10:18 +0100

    nm.sriov: extend SR-IOV implementation
    This patch extends the SR-IOV implementation in order to support VF
    configuration with nmstate. Currently, only full editation of VF/s is
    supported. Any VF/s that are emitted from the desired state are therefore
    implicitly initialized.
    
    In addition, current implementation reports the VF/s config state (from
    NM) and not the kernel state.
    
    Signed-off-by: Fernando Fernandez Mancera <ffmancera@riseup.net>

--- tests/integration/sriov_test.py
+++ tests/integration/sriov_test.py
@@ -27,7 +27,7 @@ from libnmstate.schema import Interface
 from .testlib import assertlib
 
 
-SRIOV_CONFIG = {Ethernet.SRIOV_SUBTREE: {Ethernet.SRIOV.TOTAL_VFS: 0}}
+SRIOV_CONFIG = {Ethernet.SRIOV_SUBTREE: {Ethernet.SRIOV.TOTAL_VFS: 2}}
 
 
 @pytest.mark.xfail(
@@ -55,14 +55,71 @@ def test_sriov_increase_vfs(sriov_interface):
 )
 def test_sriov_decrease_vfs(sriov_interface):
     eth_config = sriov_interface[Interface.KEY][0][Ethernet.CONFIG_SUBTREE]
-    eth_config[Ethernet.SRIOV_SUBTREE][Ethernet.SRIOV.TOTAL_VFS] = 5
+    eth_config[Ethernet.SRIOV_SUBTREE][Ethernet.SRIOV.TOTAL_VFS] = 0
     libnmstate.apply(sriov_interface)
-    eth_config[Ethernet.SRIOV_SUBTREE][Ethernet.SRIOV.TOTAL_VFS] = 2
+    assertlib.assert_state(sriov_interface)
+
+
+@pytest.mark.xfail(
+    raises=NmstateNotSupportedError,
+    reason="The device does not support SR-IOV.",
+)
+def test_sriov_create_vf_config(sriov_interface):
+    eth_config = sriov_interface[Interface.KEY][0][Ethernet.CONFIG_SUBTREE]
+    eth_config[Ethernet.SRIOV_SUBTREE][Ethernet.SRIOV.VFS_SUBTREE] = [
+        {
+            Ethernet.SRIOV.VFS.ID: 0,
+            Ethernet.SRIOV.VFS.SPOOF_CHECK: True,
+            Ethernet.SRIOV.VFS.MAC_ADDRESS: "00:11:22:33:44:55",
+            Ethernet.SRIOV.VFS.TRUST: False,
+        }
+    ]
+    libnmstate.apply(sriov_interface)
+    assertlib.assert_state(sriov_interface)
+
+
+@pytest.mark.xfail(
+    raises=NmstateNotSupportedError,
+    reason="The device does not support SR-IOV.",
+)
+def test_sriov_edit_vf_config(sriov_iface_vf):
+    eth_config = sriov_iface_vf[Interface.KEY][0][Ethernet.CONFIG_SUBTREE]
+    vf0 = eth_config[Ethernet.SRIOV_SUBTREE][Ethernet.SRIOV.VFS_SUBTREE][0]
+    vf0[Ethernet.SRIOV.VFS.TRUST] = True
+    vf0[Ethernet.SRIOV.VFS.MAC_ADDRESS] = "55:44:33:22:11:00"
+    libnmstate.apply(sriov_interface)
+    assertlib.assert_state(sriov_interface)
+
+
+@pytest.mark.xfail(
+    raises=NmstateNotSupportedError,
+    reason="The device does not support SR-IOV.",
+)
+def test_sriov_remove_vf_config(sriov_iface_vf):
+    eth_config = sriov_iface_vf[Interface.KEY][0][Ethernet.CONFIG_SUBTREE]
+    eth_config[Ethernet.SRIOV_SUBTREE][Ethernet.SRIOV.VFS_SUBTREE] = []
+    libnmstate.apply(sriov_interface)
     assertlib.assert_state(sriov_interface)
 
 
 @pytest.fixture
 def sriov_interface(eth1_up):
     eth1_up[Interface.KEY][0][Ethernet.CONFIG_SUBTREE] = SRIOV_CONFIG
+    libnmstate.apply(eth1_up)
+    yield eth1_up
+
+
+@pytest.fixture
+def sriov_iface_vf(eth1_up):
+    eth1_up[Interface.KEY][0][Ethernet.CONFIG_SUBTREE] = SRIOV_CONFIG
+    eth_config = eth1_up[Interface.KEY][0][Ethernet.CONFIG_SUBTREE]
+    eth_config[Ethernet.SRIOV_SUBTREE][Ethernet.SRIOV.VFS_SUBTREE] = [
+        {
+            Ethernet.SRIOV.VFS.ID: 0,
+            Ethernet.SRIOV.VFS.SPOOF_CHECK: True,
+            Ethernet.SRIOV.VFS.MAC_ADDRESS: "00:11:22:33:44:55",
+            Ethernet.SRIOV.VFS.TRUST: False,
+        }
+    ]
     libnmstate.apply(eth1_up)
     yield eth1_up
