eec7af648e1a7c6b06e8b9748ca3c42bddfc4c71
Fernando Fernandez Mancera <ffmancera@riseup.net>
2020-01-02 16:31:37 +0100

    version: add libnmstate.__version__
    This patch introduces libnmstate.__version__ which allow the user to get
    the current version.
    
    Signed-off-by: Fernando Fernandez Mancera <ffmancera@riseup.net>

--- packaging/get_version.sh
+++ packaging/get_version.sh
@@ -7,7 +7,7 @@ SRC_DIR="$(dirname "$0")/.."
 
 cd "$SRC_DIR"
 
-VERSION="$(cat VERSION)"
+VERSION="$(cat libnmstate/VERSION)"
 DATE="$(date +%Y%m%d)"
 
 COMMIT_COUNT="$(git rev-list --count HEAD --)"
