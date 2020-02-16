43faa8ac7050615254164790d9275ea87f3fb5af
Edward Haas <edwardh@redhat.com>
2020-02-16 08:38:21 +0200

    examples, ovs: Add OVS bond example
    Signed-off-by: Petr Horacek <phoracek@redhat.com>
    Signed-off-by: Edward Haas <edwardh@redhat.com>

--- tests/integration/examples_test.py
+++ tests/integration/examples_test.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2018-2019 Red Hat, Inc.
+# Copyright (c) 2018-2020 Red Hat, Inc.
 #
 # This file is part of nmstate
 #
@@ -53,6 +53,16 @@ def test_add_remove_ovs_bridge(eth1_up):
         assertlib.assert_state(desired_state)
 
     assertlib.assert_absent("ovs-br0")
+
+
+def test_add_remove_ovs_bridge_bond(eth1_up, eth2_up):
+    with example_state(
+        "ovsbridge_bond_create.yml", cleanup="ovsbridge_delete.yml"
+    ) as desired_state:
+        assertlib.assert_state(desired_state)
+
+    assertlib.assert_absent("ovs-br0")
+    assertlib.assert_absent("ovs-bond1")
 
 
 def test_add_remove_linux_bridge(eth1_up):
