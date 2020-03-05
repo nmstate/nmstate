d4041ad7039c022ba602caf6745e3a263c88624f
Fernando Fernandez Mancera <ffmancera@riseup.net>
2020-03-05 10:41:48 +0100

    automation: compare packaging against nmstate-0.2 branch
    Signed-off-by: Fernando Fernandez Mancera <ffmancera@riseup.net>

--- automation/run-tests.sh
+++ automation/run-tests.sh
@@ -180,7 +180,9 @@ function open_shell {
 
 # Return 0 if file is changed else 1
 function is_file_changed {
-    git diff --exit-code --name-only origin/master -- $1
+    git remote add upstream https://github.com/nmstate/nmstate.git
+    git fetch upstream
+    git diff --exit-code --name-only upstream/nmstate-0.2 -- $1
     if [ $? -eq 0 ];then
         return 1
     else
