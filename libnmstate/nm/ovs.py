52be8719b5e9bda5c980ce2a00b0a30ee74a52ff
Fernando Fernandez Mancera <ffmancera@riseup.net>
2020-04-16 15:16:14 +0200

    Revert "nm.ovs: check slave profile exists before adding it"
    This reverts commit d08174f62ff5f51cdea3dd0984d35f471e15020e as it is
    not needed anymore.
    
    Signed-off-by: Fernando Fernandez Mancera <ffmancera@riseup.net>

--- libnmstate/nm/ovs.py
+++ libnmstate/nm/ovs.py
@@ -245,6 +245,5 @@ def _get_slave_profiles(master_device, devices_info):
         if active_con:
             master = active_con.props.master
             if master and (master.get_iface() == master_device.get_iface()):
-                if active_con.props.connection:
-                    slave_profiles.append(active_con.props.connection)
+                slave_profiles.append(active_con.props.connection)
     return slave_profiles
