e0b8115cb92f75371775899621ad58d5ff96de05
Fernando Fernandez Mancera <ffmancera@riseup.net>
2021-06-30 10:35:39 +0200

    nmstate-autoconf: introduce nmstate-autoconf support
    nmstate-autoconf is a command line tool that uses libnmstate in order to
    confgure the network state automatically using LLDP information. This
    tool is experimental only.
    
    By running `nmstate-autoconf`, it will identify the interfaces connected
    to the same VLAN and will group them with a Bond interface, creating a
    host VLAN in top of the bond.
    
    The bond state will be the following:
    ```
    - name: bond50
      type: bond
      state: up
      link-aggregation:
        mode: balance-rr
        port:
        - port0
        - port1
    ```
    
    The VLAN state will be the following:
    ```
    - name: prod-net
      type: vlan
      state: up
      vlan:
        base-iface: bond50
        id: 50
    ```
    
    Integration test cases added.
    
    Signed-off-by: Fernando Fernandez Mancera <ffmancera@riseup.net>

--- setup.py
+++ setup.py
@@ -24,14 +24,26 @@ def get_version():
 
 
 def gen_manpage():
-    manpage = ""
+    nmstatectl_manpage = ""
+    autoconf_manpage = ""
     with open("doc/nmstatectl.8.in") as f:
-        manpage = f.read()
-    manpage = manpage.replace("@DATE@", date.today().strftime("%B %d, %Y"))
-    manpage = manpage.replace("@VERSION@", get_version())
+        nmstatectl_manpage = f.read()
+    with open("doc/nmstate-autoconf.8.in") as f:
+        autoconf_manpage = f.read()
+    nmstatectl_manpage = nmstatectl_manpage.replace(
+        "@DATE@", date.today().strftime("%B %d, %Y")
+    )
+    nmstatectl_manpage = nmstatectl_manpage.replace("@VERSION@", get_version())
+    autoconf_manpage = autoconf_manpage.replace(
+        "@DATE@", date.today().strftime("%B %d, %Y")
+    )
+    autoconf_manpage = autoconf_manpage.replace("@VERSION@", get_version())
+
     with open("doc/nmstatectl.8", "w") as f:
-        f.write(manpage)
-    return [("share/man/man8", ["doc/nmstatectl.8"])]
+        f.write(nmstatectl_manpage)
+    with open("doc/nmstate-autoconf.8", "w") as f:
+        f.write(autoconf_manpage)
+    return [("share/man/man8", ["doc/nmstatectl.8", "doc/nmstate-autoconf.8"])]
 
 
 setup(
@@ -47,7 +59,10 @@ setup(
     packages=find_packages(),
     install_requires=requirements(),
     entry_points={
-        "console_scripts": ["nmstatectl = nmstatectl.nmstatectl:main"]
+        "console_scripts": {
+            "nmstatectl = nmstatectl.nmstatectl:main",
+            "nmstate-autoconf = nmstatectl.nmstate_autoconf:main",
+        }
     },
     package_data={
         "libnmstate": ["schemas/operational-state.yaml", "VERSION"],
