4fe8f638ab925d5a996d1e6fb90d0c39ad72f44b
Gris Ge <fge@redhat.com>
2021-11-18 20:43:06 +0800

    rust nm: Preserve existing configurations
    Only update user desired configure and preserving previous existing
    configurations.
    
    Enabled the integration test `test_reapply_preserve_ip_config.py` for
    rust code.
    
    Signed-off-by: Gris Ge <fge@redhat.com>

--- rust/src/lib/nm/bridge.rs
+++ rust/src/lib/nm/bridge.rs
@@ -1,17 +1,20 @@
-use crate::{LinuxBridgeConfig, NmstateError};
-use nm_dbus::NmSettingBridge;
+use crate::LinuxBridgeInterface;
+use nm_dbus::NmConnection;
 
-pub(crate) fn linux_bridge_conf_to_nm(
-    br_conf: &LinuxBridgeConfig,
-) -> Result<NmSettingBridge, NmstateError> {
-    let mut nm_setting = NmSettingBridge::new();
-    if let Some(stp_enabled) = br_conf
-        .options
+pub(crate) fn gen_nm_br_setting(
+    br_iface: &LinuxBridgeInterface,
+    nm_conn: &mut NmConnection,
+) {
+    let mut nm_br_set = nm_conn.bridge.as_ref().cloned().unwrap_or_default();
+
+    if let Some(stp_enabled) = br_iface
+        .bridge
         .as_ref()
+        .and_then(|br_conf| br_conf.options.as_ref())
         .and_then(|br_opts| br_opts.stp.as_ref())
         .and_then(|stp_opts| stp_opts.enabled)
     {
-        nm_setting.stp = Some(stp_enabled);
+        nm_br_set.stp = Some(stp_enabled);
     }
-    Ok(nm_setting)
+    nm_conn.bridge = Some(nm_br_set);
 }
