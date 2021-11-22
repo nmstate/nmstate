f5d7355f743a1fb3f94c788c200ff508eb15de4f
Ales Musil <amusil@redhat.com>
2021-11-22 08:14:56 +0100

    rust, libnm_dbus: Use try_from instead of helper methods
    All those conversion errors are considered a bug because
    we should always receive correct type. There is no need
    to hace special methods as the message from direct conversion
    includes contains "incorrect type". That should be enough
    to identify that the conversion is still wrong.
    
    Signed-off-by: Ales Musil <amusil@redhat.com>

--- rust/src/libnm_dbus/connection/wired.rs
+++ rust/src/libnm_dbus/connection/wired.rs
@@ -15,15 +15,14 @@
 
 use std::collections::HashMap;
 use std::convert::TryFrom;
-use std::fmt::Write;
-
-use log::error;
 
 use serde::Deserialize;
 
 use crate::{
-    connection::DbusDictionary, dbus_value::own_value_to_bytes_array,
-    dbus_value::own_value_to_u32, error::NmError,
+    connection::DbusDictionary,
+    convert::mac_str_to_u8_array,
+    convert::{own_value_to_bytes_array, u8_array_to_mac_string},
+    error::NmError,
 };
 
 #[derive(Debug, Clone, PartialEq, Default, Deserialize)]
@@ -44,7 +43,7 @@ impl TryFrom<DbusDictionary> for NmSettingWired {
                 own_value_to_bytes_array
             )?
             .map(u8_array_to_mac_string),
-            mtu: _from_map!(v, "mtu", own_value_to_u32)?,
+            mtu: _from_map!(v, "mtu", u32::try_from)?,
             _other: v,
         })
     }
@@ -73,36 +72,4 @@ impl NmSettingWired {
     pub fn new() -> Self {
         Self::default()
     }
-}
-
-fn u8_array_to_mac_string(data: Vec<u8>) -> String {
-    let mut mac_addr = String::new();
-    for byte in &data {
-        if let Err(e) = write!(&mut mac_addr, "{:02X}:", byte) {
-            error!(
-                "Failed to convert bytes array to MAC address {:?}: {}",
-                data, e
-            );
-            return "".to_string();
-        }
-    }
-    mac_addr.pop();
-    mac_addr
-}
-
-fn mac_str_to_u8_array(mac: &str) -> Vec<u8> {
-    let mut mac_bytes = Vec::new();
-    for item in mac.split(':') {
-        match u8::from_str_radix(item, 16) {
-            Ok(i) => mac_bytes.push(i),
-            Err(e) => {
-                error!(
-                    "Failed to convert to MAC address to bytes {:?}: {}",
-                    mac, e
-                );
-                return Vec::new();
-            }
-        }
-    }
-    mac_bytes
 }
