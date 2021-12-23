2ad6a076c79e9c14fdaa1e174835b2e4ab65b200
Quique Llorente <ellorent@redhat.com>
2021-12-23 10:42:26 +0100

    ci, k8s: Bump kubernetes to 1.22
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- k8s/kubevirtci.sh
+++ k8s/kubevirtci.sh
@@ -1,23 +1,21 @@
-export KUBEVIRT_PROVIDER=${KUBEVIRT_PROVIDER:-'k8s-1.20'}
+export KUBEVIRT_PROVIDER=${KUBEVIRT_PROVIDER:-'k8s-1.22'}
+export KUBEVIRTCI_TAG='2112230038-265e857'
 
-KUBEVIRTCI_VERSION='b341c79efc15294be7a41f95229135f109304e06'
 KUBEVIRTCI_REPO='https://github.com/kubevirt/kubevirtci.git'
 KUBEVIRTCI_PATH="${PWD}/_kubevirtci"
-
-export KUBEVIRTCI_TAG='2107291018-b341c79'
 
 function kubevirtci::_get_repo() {
     git --git-dir ${KUBEVIRTCI_PATH}/.git remote get-url origin
 }
 
-function kubevirtci::_get_version() {
-    git --git-dir ${KUBEVIRTCI_PATH}/.git log --format="%H" -n 1
+function kubevirtci::_get_tag() {
+    git -C ${KUBEVIRTCI_PATH} describe --tags
 }
 
 function kubevirtci::install() {
     # Remove cloned kubevirtci repository if it does not match the requested one
     if [ -d ${KUBEVIRTCI_PATH} ]; then
-        if [ $(kubevirtci::_get_repo) != ${KUBEVIRTCI_REPO} -o $(kubevirtci::_get_version) != ${KUBEVIRTCI_VERSION} ]; then
+        if [ $(kubevirtci::_get_repo) != ${KUBEVIRTCI_REPO} -o $(kubevirtci::_get_tag) != ${KUBEVIRTCI_TAG} ]; then
             rm -rf ${KUBEVIRTCI_PATH}
         fi
     fi
@@ -26,7 +24,7 @@ function kubevirtci::install() {
         git clone ${KUBEVIRTCI_REPO} ${KUBEVIRTCI_PATH}
         (
             cd ${KUBEVIRTCI_PATH}
-            git checkout ${KUBEVIRTCI_VERSION}
+            git checkout ${KUBEVIRTCI_TAG}
         )
     fi
 }
