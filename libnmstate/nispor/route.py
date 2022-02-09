2cf9f0010bd0a91cfbd4620e8944670c9e307a6d
Gris Ge <fge@redhat.com>
2022-02-09 22:47:55 +0800

    python route: Add support of multipath route
    Supporting the rout created by below command:
    
        sudo ip -6 route add 2001:db8:e::/64 proto static \
            nexthop via 2001:db8:f::254 dev eth1 weight 1 onlink \
            nexthop via 2001:db8:f::253 dev eth1 weight 256 onlink
    
    Integration test case included.
    
    Signed-off-by: Gris Ge <fge@redhat.com>

--- libnmstate/nispor/route.py
+++ libnmstate/nispor/route.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2020 Red Hat, Inc.
+# Copyright (c) 2020-2022 Red Hat, Inc.
 #
 # This file is part of nmstate
 #
@@ -17,6 +17,8 @@
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 #
 
+from copy import deepcopy
+
 from libnmstate.schema import Route
 
 
@@ -30,24 +32,24 @@ LOCAL_ROUTE_TABLE = 255
 
 
 def nispor_route_state_to_nmstate(np_routes):
-    return [
-        _nispor_route_to_nmstate(rt)
-        for rt in np_routes
-        if rt.scope in ["universe", "link"]
-        and rt.oif != "lo"
-        and rt.table != LOCAL_ROUTE_TABLE
-    ]
+    ret = []
+    for np_route in np_routes:
+        if np_route.oif != "lo" and np_route.table != LOCAL_ROUTE_TABLE:
+            ret.extend(_nispor_route_to_nmstate(np_route))
+    return ret
 
 
 def nispor_route_state_to_nmstate_static(np_routes):
-    return [
-        _nispor_route_to_nmstate(rt)
-        for rt in np_routes
-        if rt.scope in ["universe", "link"]
-        and rt.protocol in ["static", "boot"]
-        and rt.oif != "lo"
-        and rt.table != LOCAL_ROUTE_TABLE
-    ]
+    ret = []
+    for np_route in np_routes:
+        if (
+            np_route.oif != "lo"
+            and np_route.table != LOCAL_ROUTE_TABLE
+            and np_route.scope in ["universe", "link"]
+            and np_route.protocol in ["static", "boot"]
+        ):
+            ret.extend(_nispor_route_to_nmstate(np_route))
+    return ret
 
 
 def _nispor_route_to_nmstate(np_rt):
@@ -71,10 +73,27 @@ def _nispor_route_to_nmstate(np_rt):
             else IPV4_EMPTY_NEXT_HOP_ADDRESS
         )
 
-    return {
+    nm_route = {
         Route.TABLE_ID: np_rt.table,
         Route.DESTINATION: destination,
         Route.NEXT_HOP_INTERFACE: np_rt.oif if np_rt.oif else "",
         Route.NEXT_HOP_ADDRESS: next_hop,
         Route.METRIC: np_rt.metric if np_rt.metric else 0,
     }
+    np_mp_rts = get_multipath_routes(np_rt)
+    if np_mp_rts:
+        ret = []
+        for np_mp_rt in np_mp_rts:
+            nm_route_clone = deepcopy(nm_route)
+            nm_route_clone[Route.NEXT_HOP_INTERFACE] = np_mp_rt["iface"]
+            nm_route_clone[Route.NEXT_HOP_ADDRESS] = np_mp_rt["via"]
+            ret.append(nm_route_clone)
+        return ret
+    else:
+        return [nm_route]
+
+
+# Instead of bumping nispor dependency version, we just use nispor private
+# property temporarily
+def get_multipath_routes(np_route):
+    return np_route._info.get("multipath")
