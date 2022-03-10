af6443d4804f7e518d34c97a2b86bd203425264e
Gris Ge <fge@redhat.com>
2022-03-10 17:38:31 +0800

    rust bond: Improve bond option handling
    We should merge bond options in stead of overriding.
    
    Fix the bond option verification failure, previous code ignore follow up
    verification error when got an bond option mis-match, fixed by only
    ignore bond option verification failure.
    
    Simplified the `NmSettingBond` of `libnm_dbus` as it should only
    contains NM dbus information instead of doing complex tidy up.
    
    Signed-off-by: Gris Ge <fge@redhat.com>

--- rust/src/libnm_dbus/connection/bond.rs
+++ rust/src/libnm_dbus/connection/bond.rs
@@ -10,25 +10,21 @@ use crate::{connection::DbusDictionary, NmError};
 #[serde(try_from = "DbusDictionary")]
 #[non_exhaustive]
 pub struct NmSettingBond {
-    pub mode: String,
     pub options: HashMap<String, String>,
-    _other: HashMap<String, String>,
+    _other: HashMap<String, zvariant::OwnedValue>,
 }
 
 impl TryFrom<DbusDictionary> for NmSettingBond {
     type Error = NmError;
     fn try_from(mut v: DbusDictionary) -> Result<Self, Self::Error> {
-        let mut options = HashMap::<String, String>::new();
-        let mut other = HashMap::<String, String>::new();
-
-        if let Some(value) = _from_map!(v, "options", HashMap::try_from)? {
-            options = value.clone();
-            other = value;
-        }
         Ok(Self {
-            mode: String::new(),
-            options,
-            _other: other,
+            options: _from_map!(
+                v,
+                "options",
+                <HashMap<String, String>>::try_from
+            )?
+            .unwrap_or_default(),
+            _other: v,
         })
     }
 }
@@ -38,49 +34,24 @@ impl NmSettingBond {
         Self::default()
     }
 
+    pub(crate) fn to_value(
+        &self,
+    ) -> Result<HashMap<&str, zvariant::Value>, NmError> {
+        let mut ret = HashMap::new();
+        ret.insert("options", zvariant::Value::from(self.options.clone()));
+        ret.extend(self._other.iter().map(|(key, value)| {
+            (key.as_str(), zvariant::Value::from(value.clone()))
+        }));
+        Ok(ret)
+    }
+
     pub(crate) fn to_keyfile(
         &self,
     ) -> Result<HashMap<String, zvariant::Value>, NmError> {
         let mut ret = HashMap::new();
-        ret.insert("mode".to_string(), Value::new(self.mode.as_str()));
         for (key, value) in self.options.iter() {
             ret.insert(key.to_string(), Value::new(value));
         }
         Ok(ret)
-    }
-
-    pub(crate) fn to_value(&self) -> Result<HashMap<&str, Value>, NmError> {
-        let mut merged_opts = self.options.clone();
-        let mut ret = HashMap::new();
-
-        // Do not merge existing options if the user specified empty options
-        merged_opts.extend(
-            self._other
-                .iter()
-                .filter(|(k, _)| !self.options.contains_key(*k))
-                .map(|(k, v)| (k.to_string(), v.to_string())),
-        );
-        if let Some(arp_ip_target) = self.options.get("arp_ip_target") {
-            if arp_ip_target.is_empty() {
-                merged_opts.remove("arp_ip_target");
-            }
-        }
-
-        if !self.mode.is_empty() {
-            merged_opts.insert("mode".to_string(), self.mode.clone());
-        }
-
-        if !merged_opts.is_empty() {
-            ret.insert("options", Value::from(merged_opts.clone()));
-        }
-        Ok(ret)
-    }
-
-    pub fn clear_existing_opts(&mut self) {
-        self._other.clear();
-    }
-
-    pub fn get_current_mode(&self) -> Option<&String> {
-        self._other.get(&String::from("mode"))
     }
 }
