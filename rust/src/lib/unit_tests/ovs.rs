e0118a632f2b52864846038e191ecae6c1fb3709
Gris Ge <fge@redhat.com>
2022-03-12 09:10:07 +0800

    rust: Support ignore interface when applying
    Support `state: ignore` as python API did.
    
    Unit test case included.
    
    Enabled all integration test case for linux bridge.
    
    Signed-off-by: Gris Ge <fge@redhat.com>

---
+++ rust/src/lib/unit_tests/ovs.rs
@@ -0,0 +1,110 @@
+use crate::{InterfaceType, Interfaces};
+
+#[test]
+fn test_ovs_bridge_ignore_port() {
+    let mut ifaces: Interfaces = serde_yaml::from_str(
+        r#"---
+- name: eth1
+  type: ethernet
+  state: ignore
+- name: eth2
+  type: ethernet
+  state: up
+- name: br0
+  type: ovs-bridge
+  state: up
+  bridge:
+    port:
+    - name: eth2
+"#,
+    )
+    .unwrap();
+    let mut cur_ifaces: Interfaces = serde_yaml::from_str(
+        r#"---
+- name: eth1
+  type: ethernet
+  state: up
+- name: eth2
+  type: ethernet
+  state: up
+- name: br0
+  type: ovs-bridge
+  state: up
+  bridge:
+    port:
+    - name: eth1
+    - name: eth2
+"#,
+    )
+    .unwrap();
+
+    let ignored_kernel_ifaces = ifaces.ignored_kernel_iface_names();
+
+    assert_eq!(ignored_kernel_ifaces, vec!["eth1".to_string()]);
+
+    let ignored_user_ifaces = ifaces.ignored_user_iface_name_types();
+    assert!(ignored_user_ifaces.is_empty());
+
+    ifaces.remove_ignored_ifaces(&ignored_kernel_ifaces, &ignored_user_ifaces);
+    cur_ifaces
+        .remove_ignored_ifaces(&ignored_kernel_ifaces, &ignored_user_ifaces);
+
+    let (add_ifaces, chg_ifaces, del_ifaces) =
+        ifaces.gen_state_for_apply(&cur_ifaces, false).unwrap();
+
+    assert!(!ifaces.kernel_ifaces.contains_key("eth1"));
+    assert!(!cur_ifaces.kernel_ifaces.contains_key("eth1"));
+    assert_eq!(
+        ifaces.user_ifaces[&("br0".to_string(), InterfaceType::OvsBridge)]
+            .ports(),
+        Some(vec!["eth2"])
+    );
+    assert_eq!(
+        cur_ifaces.user_ifaces[&("br0".to_string(), InterfaceType::OvsBridge)]
+            .ports(),
+        Some(vec!["eth2"])
+    );
+    assert!(!add_ifaces.kernel_ifaces.contains_key("eth1"));
+    assert!(!chg_ifaces.kernel_ifaces.contains_key("eth1"));
+    assert!(!del_ifaces.kernel_ifaces.contains_key("eth1"));
+}
+
+#[test]
+fn test_ovs_bridge_verify_ignore_port() {
+    let ifaces: Interfaces = serde_yaml::from_str(
+        r#"---
+- name: eth1
+  type: ethernet
+  state: ignore
+- name: eth2
+  type: ethernet
+- name: br0
+  type: ovs-bridge
+  state: up
+  bridge:
+    port:
+    - name: eth2
+"#,
+    )
+    .unwrap();
+    let cur_ifaces: Interfaces = serde_yaml::from_str(
+        r#"---
+- name: eth1
+  type: ethernet
+  state: up
+- name: eth2
+  type: ethernet
+  state: up
+- name: br0
+  type: ovs-bridge
+  state: up
+  bridge:
+    port:
+    - name: eth1
+    - name: eth2
+"#,
+    )
+    .unwrap();
+
+    ifaces.verify(&cur_ifaces).unwrap();
+}
