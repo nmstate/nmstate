9b30b28e7fe223ad50693f4e0b1e2718c7b0ea7d
Fernando Fernandez Mancera <ffmancera@riseup.net>
2022-04-21 16:28:50 +0200

    rust: normalize MAC address before verification
    When verifying, Nmstate should convert the MAC address to uppercase to
    avoid verification errors.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=2048051
    
    Signed-off-by: Fernando Fernandez Mancera <ffmancera@riseup.net>

--- rust/src/lib/unit_tests/base.rs
+++ rust/src/lib/unit_tests/base.rs
@@ -12,3 +12,17 @@ accept-all-mac-addresses: "true"
     .unwrap();
     assert_eq!(iface.accept_all_mac_addresses, Some(true));
 }
+
+#[test]
+fn test_base_iface_mac_address_uppercase_before_verification() {
+    let mut iface: BaseInterface = serde_yaml::from_str(
+        r#"
+name: "eth1"
+mtu: "1500"
+mac-address: "d4:ee:07:25:42:5a"
+"#,
+    )
+    .unwrap();
+    iface.pre_verify_cleanup();
+    assert_eq!(iface.mac_address, Some(String::from("D4:EE:07:25:42:5A")));
+}
