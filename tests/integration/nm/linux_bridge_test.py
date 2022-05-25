2f18ee4064f917be40381f4aef8b5ca8c5c68a75
Gris Ge <fge@redhat.com>
2022-05-25 21:00:47 +0800

    test: conditional xfail of test_linux_bridge_does_not_lose_unmanaged_port_on_rollback
    The NetworkManager 1.39.3+ has fixed this issue.
    Also rebuild CI container to pull the latest rpm in.
    
    Signed-off-by: Gris Ge <fge@redhat.com>

--- tests/integration/nm/linux_bridge_test.py
+++ tests/integration/nm/linux_bridge_test.py
@@ -34,6 +34,7 @@ from ..testlib.bondlib import bond_interface
 from ..testlib.bridgelib import linux_bridge
 from ..testlib.cmdlib import exec_cmd
 from ..testlib.dummy import nm_unmanaged_dummy
+from ..testlib.env import nm_minor_micro_version
 from ..testlib.statelib import show_only
 
 
@@ -238,6 +239,7 @@ def bridge_with_unmanaged_port(eth1_up, unmanged_veth0):
 
 @pytest.mark.tier1
 @pytest.mark.xfail(
+    nm_minor_micro_version() <= 39.3,
     raises=AssertionError,
     reason="https://bugzilla.redhat.com/2076131",
     strict=True,
