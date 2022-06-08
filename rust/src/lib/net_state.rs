fb5e460d7b258444e3b24df2017b81c1c591be82
Gris Ge <fge@redhat.com>
2022-06-08 22:50:12 +0800

    gen_conf: Change the API to align with previous python API
    In nmstate 1.x and 2.0, the `libnmstate.generate_configurations()` is
    returning:
        HashMap<String, Vec<(String, String)>>
    
    So the rust API should be.
    
    Fixed python binding also.
    
    Signed-off-by: Gris Ge <fge@redhat.com>

--- rust/src/lib/net_state.rs
+++ rust/src/lib/net_state.rs
@@ -377,7 +377,7 @@ impl NetworkState {
 
     pub fn gen_conf(
         &self,
-    ) -> Result<HashMap<String, Vec<String>>, NmstateError> {
+    ) -> Result<HashMap<String, Vec<(String, String)>>, NmstateError> {
         let mut ret = HashMap::new();
         let mut self_clone = self.clone();
         self_clone.interfaces.set_unknown_iface_to_eth();
