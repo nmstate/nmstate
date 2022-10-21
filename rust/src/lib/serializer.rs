6a6184ae78d6fd9fa4df02226cdff0e075807dbb
Fernando Fernandez Mancera <ffmancera@riseup.net>
2022-10-21 13:18:41 +0200

    serializer: add option_u32_as_hex serializer helper
    There are options that will need to be serialized as hexadecimal to be
    more user friendly. This serializer helper is needed for the fwmark and
    fwmask support.
    
    Signed-off-by: Fernando Fernandez Mancera <ffmancera@riseup.net>

--- rust/src/lib/serializer.rs
+++ rust/src/lib/serializer.rs
@@ -1,7 +1,23 @@
+use serde::Serializer;
+
 pub(crate) fn is_option_string_empty(data: &Option<String>) -> bool {
     if let Some(s) = data {
         s.is_empty()
     } else {
         true
+    }
+}
+
+pub(crate) fn option_u32_as_hex<S>(
+    data: &Option<u32>,
+    serializer: S,
+) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    if let Some(v) = data {
+        serializer.serialize_str(format!("{:#x?}", v).as_str())
+    } else {
+        serializer.serialize_none()
     }
 }
