4dc5ac73522b6dd9d84bde1824aef73d838548ca
Gris Ge <fge@redhat.com>
2022-11-10 15:51:25 +0800

    ip: Preserve the IP address order when applying
    When applying the IP address, we should preserve the order for use case
    whether user is expecting non-first ones been set with `secondary` flag.
    
    In RHEL/CentOS 8, NetworkManager is using reverted IPv6 address
    according to
    https://bugzilla.redhat.com/show_bug.cgi?id=2139443
    
    Hence downstream nmstate will ship additional patch to fix it.
    The upstream nmstate will not revert the IPv6 address list before
    sending to NM.
    
    The downstream build of RHEL 8 has different behaviour than copr build
    from git main branch. It is hard to tell whether we are using downstream
    build or git build at runtime, hence we ship the
    `test_preserve_ipv6_addresses_order` test in RHEL 8.
    
    Integration test case included.
    
    Signed-off-by: Gris Ge <fge@redhat.com>
    (cherry picked from commit 2d0cfd5ad8e049f30cad10d977a5fae8bc4e6b64)

--- tests/integration/static_ip_address_test.py
+++ tests/integration/static_ip_address_test.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2018-2019 Red Hat, Inc.
+# Copyright (c) 2018-2022 Red Hat, Inc.
 #
 # This file is part of nmstate
 #
@@ -31,6 +31,8 @@ from .testlib import assertlib
 from .testlib import cmdlib
 from .testlib import statelib
 from .testlib.dummy import nm_unmanaged_dummy
+from .testlib.env import is_el8
+from .testlib.iproutelib import iproute_get_ip_addrs_with_order
 from .testlib.iproutelib import ip_monitor_assert_stable_link_up
 
 # TEST-NET addresses: https://tools.ietf.org/html/rfc5737#section-3
@@ -724,3 +726,66 @@ def test_preserve_ip_conf_if_not_mentioned(setup_eth1_static_ip):
         }
     )
     assertlib.assert_state_match(desired_state)
+
+
+def test_preserve_ipv4_addresses_order(eth1_up):
+    desired_state = {
+        Interface.KEY: [
+            {
+                Interface.NAME: "eth1",
+                Interface.TYPE: InterfaceType.ETHERNET,
+                Interface.STATE: InterfaceState.UP,
+                Interface.IPV4: {
+                    InterfaceIPv4.ENABLED: True,
+                    InterfaceIPv4.ADDRESS: [
+                        {
+                            InterfaceIPv4.ADDRESS_IP: IPV4_ADDRESS2,
+                            InterfaceIPv4.ADDRESS_PREFIX_LENGTH: 24,
+                        },
+                        {
+                            InterfaceIPv4.ADDRESS_IP: IPV4_ADDRESS1,
+                            InterfaceIPv4.ADDRESS_PREFIX_LENGTH: 24,
+                        },
+                    ],
+                },
+            }
+        ]
+    }
+    libnmstate.apply(desired_state)
+    ip_addrs = iproute_get_ip_addrs_with_order(iface="eth1", is_ipv6=False)
+    assert ip_addrs[0] == IPV4_ADDRESS2
+    assert ip_addrs[1] == IPV4_ADDRESS1
+
+
+@pytest.mark.skipif(
+    is_el8(),
+    reason="RHEL 8 hold different IPv6 address order in rpm between "
+    "downstream shipped and copr main branch built",
+)
+def test_preserve_ipv6_addresses_order(eth1_up):
+    desired_state = {
+        Interface.KEY: [
+            {
+                Interface.NAME: "eth1",
+                Interface.TYPE: InterfaceType.ETHERNET,
+                Interface.STATE: InterfaceState.UP,
+                Interface.IPV6: {
+                    InterfaceIPv6.ENABLED: True,
+                    InterfaceIPv6.ADDRESS: [
+                        {
+                            InterfaceIPv6.ADDRESS_IP: IPV6_ADDRESS2,
+                            InterfaceIPv6.ADDRESS_PREFIX_LENGTH: 64,
+                        },
+                        {
+                            InterfaceIPv6.ADDRESS_IP: IPV6_ADDRESS1,
+                            InterfaceIPv6.ADDRESS_PREFIX_LENGTH: 64,
+                        },
+                    ],
+                },
+            }
+        ]
+    }
+    libnmstate.apply(desired_state)
+    ip_addrs = iproute_get_ip_addrs_with_order(iface="eth1", is_ipv6=True)
+    assert ip_addrs[0] == IPV6_ADDRESS2
+    assert ip_addrs[1] == IPV6_ADDRESS1
