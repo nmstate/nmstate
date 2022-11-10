18607cf751119cb3d85669556886c2e54ed5de49
Gris Ge <fge@redhat.com>
2022-11-10 15:51:25 +0800

    ip: Preserve the IP address order when applying
    When applying the IP address, we should preserve the order for use case
    whether user is expecting non-first ones been set with `secondary` flag.
    
    In RHEL/CentOS 8, NetworkManager is using reverted IPv6 address
    according to
    https://bugzilla.redhat.com/show_bug.cgi?id=2139443
    
    Hence downstream nmstate will ship additional patch to fix it.
    The upstream nmstate will not revert the IPv6 address list before
    sending to NM.
    
    The downstream build of RHEL 8 has different behaviour than copr build
    from git main branch. It is hard to tell whether we are using downstream
    build or git build at runtime, hence we ship the
    `test_preserve_ipv6_addresses_order` test in RHEL 8.
    
    Integration test case included.
    
    Signed-off-by: Gris Ge <fge@redhat.com>
    (cherry picked from commit 2d0cfd5ad8e049f30cad10d977a5fae8bc4e6b64)
    (cherry picked from commit 4dc5ac73522b6dd9d84bde1824aef73d838548ca)

--- tests/integration/testlib/env.py
+++ tests/integration/testlib/env.py
@@ -18,6 +18,7 @@
 #
 
 import os
+from .cmdlib import exec_cmd
 
 import gi
 
@@ -38,3 +39,11 @@ def is_ubuntu_kernel():
 
 def nm_major_minor_version():
     return float(f"{NM.MAJOR_VERSION}.{NM.MINOR_VERSION}")
+
+
+def is_k8s():
+    return os.getenv("RUN_K8S") == "true"
+
+
+def is_el8():
+    return exec_cmd("rpm -E %{?rhel}".split())[1].strip() == "8"
