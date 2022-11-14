2a16c1fa39f98bd544bf0efc34ef07093d58b3e0
Gris Ge <fge@redhat.com>
2022-11-14 15:38:34 +0800

    Rust doc for dummy interface
    Signed-off-by: Gris Ge <fge@redhat.com>

--- rust/src/lib/ifaces/dummy.rs
+++ rust/src/lib/ifaces/dummy.rs
@@ -4,6 +4,47 @@ use crate::{BaseInterface, InterfaceType};
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 #[non_exhaustive]
+/// Dummy interface. Only contain information of [BaseInterface].
+/// Example yaml outpuf of `[crate::NetworkState]` with dummy interface:
+/// ```yml
+/// interfaces:
+/// - name: dummy1
+///   type: dummy
+///   state: up
+///   mac-address: BE:25:F0:6D:55:64
+///   mtu: 1500
+///   wait-ip: any
+///   ipv4:
+///     enabled: false
+///   ipv6:
+///     enabled: false
+///   accept-all-mac-addresses: false
+///   lldp:
+///     enabled: false
+///   ethtool:
+///     feature:
+///       tx-checksum-ip-generic: true
+///       tx-ipxip6-segmentation: true
+///       rx-gro: true
+///       tx-generic-segmentation: true
+///       tx-udp-segmentation: true
+///       tx-udp_tnl-csum-segmentation: true
+///       rx-udp-gro-forwarding: false
+///       tx-tcp-segmentation: true
+///       tx-sctp-segmentation: true
+///       tx-ipxip4-segmentation: true
+///       tx-nocache-copy: false
+///       tx-gre-csum-segmentation: true
+///       tx-udp_tnl-segmentation: true
+///       tx-tcp-mangleid-segmentation: true
+///       rx-gro-list: false
+///       tx-scatter-gather-fraglist: true
+///       tx-gre-segmentation: true
+///       tx-tcp-ecn-segmentation: true
+///       tx-gso-list: true
+///       highdma: true
+///       tx-tcp6-segmentation: true
+/// ```
 pub struct DummyInterface {
     #[serde(flatten)]
     pub base: BaseInterface,
