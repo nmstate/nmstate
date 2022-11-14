767f3b0c852f6633974cc5b057ecf27f89d5c00e
Gris Ge <fge@redhat.com>
2022-11-14 15:53:14 +0800

    Rust doc for ethtool
    Signed-off-by: Gris Ge <fge@redhat.com>

--- rust/src/lib/ifaces/ethtool.rs
+++ rust/src/lib/ifaces/ethtool.rs
@@ -30,18 +30,53 @@ pub type EthtoolFeatureConfig = HashMap<String, bool>;
 #[derive(Serialize, Deserialize, Debug, Eq, PartialEq, Clone, Default)]
 #[non_exhaustive]
 #[serde(deny_unknown_fields)]
+/// The ethtool configurations.
+/// The yaml output of [crate::NetworkState] containing ethtool information of
+/// an ethernet interface would be:
+/// ```yml
+/// interfaces:
+/// - name: ens3
+///   type: ethernet
+///   state: up
+///   ethtool:
+///     feature:
+///       tx-tcp-ecn-segmentation: true
+///       tx-tcp-mangleid-segmentation: false
+///       tx-tcp6-segmentation: true
+///       tx-tcp-segmentation: true
+///       rx-gro-list: false
+///       rx-udp-gro-forwarding: false
+///       rx-gro-hw: true
+///       tx-checksum-ip-generic: true
+///       tx-generic-segmentation: true
+///       rx-gro: true
+///       tx-nocache-copy: false
+///     coalesce:
+///       rx-frames: 1
+///       tx-frames: 1
+///     ring:
+///       rx: 256
+///       rx-max: 256
+///       tx: 256
+///       tx-max: 256
+/// ```
 pub struct EthtoolConfig {
     #[serde(skip_serializing_if = "Option::is_none")]
+    /// The pause parameters of the specified Ethernet device.
     pub pause: Option<EthtoolPauseConfig>,
     #[serde(
         skip_serializing_if = "Option::is_none",
         default,
         deserialize_with = "parse_ethtool_feature"
     )]
+    /// The protocol offload and other features of specified network device.
+    /// Only changeable features are included when querying.
     pub feature: Option<EthtoolFeatureConfig>,
     #[serde(skip_serializing_if = "Option::is_none")]
+    /// The coalescing settings of the specified network device.
     pub coalesce: Option<EthtoolCoalesceConfig>,
     #[serde(skip_serializing_if = "Option::is_none")]
+    /// The rx/tx ring parameters of the specified network device.
     pub ring: Option<EthtoolRingConfig>,
 }
 
@@ -109,132 +144,154 @@ pub struct EthtoolCoalesceConfig {
         default,
         deserialize_with = "crate::deserializer::option_bool_or_string"
     )]
+    /// Deserialize and serialize from/to `adaptive-rx`.
     pub adaptive_rx: Option<bool>,
     #[serde(
         skip_serializing_if = "Option::is_none",
         default,
         deserialize_with = "crate::deserializer::option_bool_or_string"
     )]
+    /// Deserialize and serialize from/to `adaptive-tx`.
     pub adaptive_tx: Option<bool>,
     #[serde(
         skip_serializing_if = "Option::is_none",
         default,
         deserialize_with = "crate::deserializer::option_u32_or_string"
     )]
+    /// Deserialize and serialize from/to `pkt-rate-high`.
     pub pkt_rate_high: Option<u32>,
     #[serde(
         skip_serializing_if = "Option::is_none",
         default,
         deserialize_with = "crate::deserializer::option_u32_or_string"
     )]
+    /// Deserialize and serialize from/to `pkt-rate-low`.
     pub pkt_rate_low: Option<u32>,
     #[serde(
         skip_serializing_if = "Option::is_none",
         default,
         deserialize_with = "crate::deserializer::option_u32_or_string"
     )]
+    /// Deserialize and serialize from/to `rx-frames`.
     pub rx_frames: Option<u32>,
     #[serde(
         skip_serializing_if = "Option::is_none",
         default,
         deserialize_with = "crate::deserializer::option_u32_or_string"
     )]
+    /// Deserialize and serialize from/to `rx-frames-high`.
     pub rx_frames_high: Option<u32>,
     #[serde(
         skip_serializing_if = "Option::is_none",
         default,
         deserialize_with = "crate::deserializer::option_u32_or_string"
     )]
+    /// Deserialize and serialize from/to `rx-frames-irq`.
     pub rx_frames_irq: Option<u32>,
     #[serde(
         skip_serializing_if = "Option::is_none",
         default,
         deserialize_with = "crate::deserializer::option_u32_or_string"
     )]
+    /// Deserialize and serialize from/to `rx-frames-low`.
     pub rx_frames_low: Option<u32>,
     #[serde(
         skip_serializing_if = "Option::is_none",
         default,
         deserialize_with = "crate::deserializer::option_u32_or_string"
     )]
+    /// Deserialize and serialize from/to `rx-usecs`.
     pub rx_usecs: Option<u32>,
     #[serde(
         skip_serializing_if = "Option::is_none",
         default,
         deserialize_with = "crate::deserializer::option_u32_or_string"
     )]
+    /// Deserialize and serialize from/to `rx-usecs-high`.
     pub rx_usecs_high: Option<u32>,
     #[serde(
         skip_serializing_if = "Option::is_none",
         default,
         deserialize_with = "crate::deserializer::option_u32_or_string"
     )]
+    /// Deserialize and serialize from/to `rx-usecs-irq`.
     pub rx_usecs_irq: Option<u32>,
     #[serde(
         skip_serializing_if = "Option::is_none",
         default,
         deserialize_with = "crate::deserializer::option_u32_or_string"
     )]
+    /// Deserialize and serialize from/to `rx-usecs-low`.
     pub rx_usecs_low: Option<u32>,
     #[serde(
         skip_serializing_if = "Option::is_none",
         default,
         deserialize_with = "crate::deserializer::option_u32_or_string"
     )]
+    /// Deserialize and serialize from/to `sample-interval`.
     pub sample_interval: Option<u32>,
     #[serde(
         skip_serializing_if = "Option::is_none",
         default,
         deserialize_with = "crate::deserializer::option_u32_or_string"
     )]
+    /// Deserialize and serialize from/to `stats-block-usecs`.
     pub stats_block_usecs: Option<u32>,
     #[serde(
         skip_serializing_if = "Option::is_none",
         default,
         deserialize_with = "crate::deserializer::option_u32_or_string"
     )]
+    /// Deserialize and serialize from/to `tx-frames`.
     pub tx_frames: Option<u32>,
     #[serde(
         skip_serializing_if = "Option::is_none",
         default,
         deserialize_with = "crate::deserializer::option_u32_or_string"
     )]
+    /// Deserialize and serialize from/to `tx-frames-high`.
     pub tx_frames_high: Option<u32>,
     #[serde(
         skip_serializing_if = "Option::is_none",
         default,
         deserialize_with = "crate::deserializer::option_u32_or_string"
     )]
+    /// Deserialize and serialize from/to `tx-frames-irq`.
     pub tx_frames_irq: Option<u32>,
     #[serde(
         skip_serializing_if = "Option::is_none",
         default,
         deserialize_with = "crate::deserializer::option_u32_or_string"
     )]
+    /// Deserialize and serialize from/to `tx-frames-low`.
     pub tx_frames_low: Option<u32>,
     #[serde(
         skip_serializing_if = "Option::is_none",
         default,
         deserialize_with = "crate::deserializer::option_u32_or_string"
     )]
+    /// Deserialize and serialize from/to `tx-usecs`.
     pub tx_usecs: Option<u32>,
     #[serde(
         skip_serializing_if = "Option::is_none",
         default,
         deserialize_with = "crate::deserializer::option_u32_or_string"
     )]
+    /// Deserialize and serialize from/to `tx-usecs-high`.
     pub tx_usecs_high: Option<u32>,
     #[serde(
         skip_serializing_if = "Option::is_none",
         default,
         deserialize_with = "crate::deserializer::option_u32_or_string"
     )]
+    /// Deserialize and serialize from/to `tx-usecs-irq`.
     pub tx_usecs_irq: Option<u32>,
     #[serde(
         skip_serializing_if = "Option::is_none",
         default,
         deserialize_with = "crate::deserializer::option_u32_or_string"
     )]
+    /// Deserialize and serialize from/to `tx-usecs-low`.
     pub tx_usecs_low: Option<u32>,
 }
 
@@ -259,30 +316,35 @@ pub struct EthtoolRingConfig {
         default,
         deserialize_with = "crate::deserializer::option_u32_or_string"
     )]
+    /// Deserialize and serialize from/to `rx-max`.
     pub rx_max: Option<u32>,
     #[serde(
         skip_serializing_if = "Option::is_none",
         default,
         deserialize_with = "crate::deserializer::option_u32_or_string"
     )]
+    /// Deserialize and serialize from/to `rx-jumbo`.
     pub rx_jumbo: Option<u32>,
     #[serde(
         skip_serializing_if = "Option::is_none",
         default,
         deserialize_with = "crate::deserializer::option_u32_or_string"
     )]
+    /// Deserialize and serialize from/to `rx-jumbo-max`.
     pub rx_jumbo_max: Option<u32>,
     #[serde(
         skip_serializing_if = "Option::is_none",
         default,
         deserialize_with = "crate::deserializer::option_u32_or_string"
     )]
+    /// Deserialize and serialize from/to `rx-mini`.
     pub rx_mini: Option<u32>,
     #[serde(
         skip_serializing_if = "Option::is_none",
         default,
         deserialize_with = "crate::deserializer::option_u32_or_string"
     )]
+    /// Deserialize and serialize from/to `rx-mini-max`.
     pub rx_mini_max: Option<u32>,
     #[serde(
         skip_serializing_if = "Option::is_none",
@@ -295,6 +357,7 @@ pub struct EthtoolRingConfig {
         default,
         deserialize_with = "crate::deserializer::option_u32_or_string"
     )]
+    /// Deserialize and serialize from/to `tx-max`.
     pub tx_max: Option<u32>,
 }
 
