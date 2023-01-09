0b2deca7a8da55454fb26423b16fc329f5e8122f
Gris Ge <fge@redhat.com>
2023-01-09 17:13:12 +0800

    Bump version to 2.2.4
    Signed-off-by: Gris Ge <fge@redhat.com>

--- rust/src/python/libnmstate/__init__.py
+++ rust/src/python/libnmstate/__init__.py
@@ -20,6 +20,6 @@ __all__ = [
     "show_running_config",
 ]
 
-__version__ = "2.2.3"
+__version__ = "2.2.4"
 
 BASE_ON_RUST = True
--- rust/src/python/setup.py
+++ rust/src/python/setup.py
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
 import setuptools
 
 
@@ -13,7 +15,7 @@ def requirements():
 
 setuptools.setup(
     name="nmstate",
-    version="2.2.3",
+    version="2.2.4",
     author="Gris Ge",
     author_email="fge@redhat.com",
     description="Python binding of nmstate",
