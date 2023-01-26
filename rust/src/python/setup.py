32d112ad2402e6210bc11175cb8496c838ff7c3e
Fernando Fernandez Mancera <ffmancera@riseup.net>
2023-01-26 11:50:50 +0100

    VERSION: Bump version to 2.2.6
    Signed-off-by: Fernando Fernandez Mancera <ffmancera@riseup.net>

--- rust/src/python/setup.py
+++ rust/src/python/setup.py
@@ -15,7 +15,7 @@ def requirements():
 
 setuptools.setup(
     name="nmstate",
-    version="2.2.5",
+    version="2.2.6",
     author="Gris Ge",
     author_email="fge@redhat.com",
     description="Python binding of nmstate",
