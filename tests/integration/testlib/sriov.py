c59032746911b29fdf7fd677d7bb10f7187a7024
Gris Ge <fge@redhat.com>
2023-03-21 11:15:33 +0800

    test: Fix `test_wait_sriov_vf_been_created` on mlx5 NIC
    On some `mlx5_core` NIC, the `ip -d link show <pf_name>` does not have
    VF interface name, which will fail the `test_wait_sriov_vf_been_created`
    for VF name not found.
    
    The fix would be changed the test code to use this folder for VF
    interface name as the production code:
    
        /sys/class/net/{pf_name}/device/virtfn{sriov_id}/net/
    
    Manually test on mlx5 MT27710 NIC.
    
    Signed-off-by: Gris Ge <fge@redhat.com>

--- tests/integration/testlib/sriov.py
+++ tests/integration/testlib/sriov.py
@@ -1,22 +1,11 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
-import json
-
-import libnmstate
-from libnmstate.schema import Interface
-
-from .cmdlib import exec_cmd
+import glob
+import os
 
 
 def get_sriov_vf_names(pf_name):
-    output = exec_cmd(f"ip -j -d link show {pf_name}".split())[1]
-    link_info = json.loads(output)[0]
-    macs = [
-        vf_info["address"].upper()
-        for vf_info in link_info.get("vfinfo_list", [])
-    ]
-    return [
-        iface[Interface.NAME]
-        for iface in libnmstate.show()[Interface.KEY]
-        if iface[Interface.MAC] in macs
-    ]
+    ret = []
+    for folder in glob.glob(f"/sys/class/net/{pf_name}/device/virtfn*/net/"):
+        ret.extend(os.listdir(folder))
+    return ret
