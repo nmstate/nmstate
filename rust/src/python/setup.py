bfd4e34b1fbac9d93f9435782e0cccfc3cdbffe0
Gris Ge <fge@redhat.com>
2023-03-29 17:42:17 +0800

    New release 1.4.3
    === Breaking changes
     - N/A
    
    === New features
     - Introduced YAML support into rust C binding. (c73bff8d)
     - Support NetworkManager global DNS. (155a4cfe)
    
    === Bug fixes
     - Fix SR-IOV problem. (4acfc16d)
     - Ignore error when creating profile if not desired. (ab45fb81)
     - Fix typo in supported ethtool features tx-gro -> rx-gro. (dd8fa7c7)
     - Preserver IP when converting unmanaged interface to managed. (b4c30ab0)
     - Fix error when server has static DNS search with auto DNS server. (647834df)
    
    Signed-off-by: Gris Ge <fge@redhat.com>

--- rust/src/python/setup.py
+++ rust/src/python/setup.py
@@ -15,7 +15,7 @@ def requirements():
 
 setuptools.setup(
     name="nmstate",
-    version="1.4.1",
+    version="1.4.3",
     author="Gris Ge",
     author_email="fge@redhat.com",
     description="Python binding of nmstate",
