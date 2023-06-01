2f27e33bc828013cf87d7015c5043e92ad77085e
Fernando Fernandez Mancera <ffmancera@riseup.net>
2023-06-01 11:39:38 +0200

    VERSION: Bump to 2.2.13
    Signed-off-by: Fernando Fernandez Mancera <ffmancera@riseup.net>

--- rust/src/python/libnmstate/__init__.py
+++ rust/src/python/libnmstate/__init__.py
@@ -20,6 +20,6 @@ __all__ = [
     "show_running_config",
 ]
 
-__version__ = "2.2.12"
+__version__ = "2.2.13"
 
 BASE_ON_RUST = True
--- rust/src/python/setup.py
+++ rust/src/python/setup.py
@@ -15,7 +15,7 @@ def requirements():
 
 setuptools.setup(
     name="nmstate",
-    version="2.2.12",
+    version="2.2.13",
     author="Gris Ge",
     author_email="fge@redhat.com",
     description="Python binding of nmstate",
