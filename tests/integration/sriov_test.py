nmstate/nmstate

commit 20871bac631c216a00950c091916a7845ce32a43

sriov: Support enabling SR-IOV and use future VF in single desire state
When changing SR-IOV to add more VF interface, this patch allows user to
crate VF and use it in single desire state, for example with SR-IOV
disabled on eth1, you can apply this state:

```yml
---
interfaces:
  - name: sriov:eth1:1
    type: ethernet
    state: up
    ipv4:
      enabled: false
    ipv6:
      enabled: false
  - name: eth1
    type: ethernet
    state: up
    ethernet:
      sr-iov:
        total-vfs: 2
```

The implementation detail is introducing
`MergedNetworkState.isolate_sriov_conf_out()` to __move__ ethernet
section to new `MergedNetworkState` if SR-IOV config desired. This new
PF only state will be applied first and the follow-up verification
process will wait all VF shows up before proceed the original
MergedNetworkState.

Unit test case and integration test case included.

Signed-off-by: Gris Ge <fge@redhat.com>

rust/src/lib/query_apply/ethernet.rs
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::{
-    ErrorKind, EthernetConfig, EthernetInterface, Interface, Interfaces,
-    NmstateError, SrIovConfig, VethConfig,
+    ErrorKind, EthernetConfig, EthernetInterface, Interface, InterfaceType,
+    Interfaces, MergedInterface, MergedNetworkState, NmstateError, SrIovConfig,
+    VethConfig,
 };
 
 impl EthernetInterface {
@@ -97,5 +98,58 @@ impl Interfaces {
             }
         }
         Ok(())
+    }
+}
+
+impl MergedNetworkState {
+    // Return newly create MergedNetworkState containing only ethernet section
+    // of interface with SR-IOV PF changes.
+    // The self will have SR-IOV PF change removed.
+    pub(crate) fn isolate_sriov_conf_out(&mut self) -> Option<Self> {
+        let mut pf_ifaces: Vec<MergedInterface> = Vec::new();
+
+        for iface in self.interfaces.kernel_ifaces.values_mut().filter(|i| {
+            i.is_desired() && i.merged.iface_type() == InterfaceType::Ethernet
+        }) {
+            if let Some(Interface::Ethernet(apply_iface)) =
+                iface.for_apply.as_mut()
+            {
+                if let Some(true) =
+                    apply_iface.ethernet.as_ref().map(|e| e.sr_iov.is_some())
+                {
+                    if let Some(eth_conf) = apply_iface.ethernet.as_ref() {
+                        let new_iface = EthernetInterface {
+                            base: apply_iface.base.clone_name_type_only(),
+                            ethernet: Some(eth_conf.clone()),
+                            ..Default::default()
+                        };
+                        if let Ok(new_merged_iface) = MergedInterface::new(
+                            Some(Interface::Ethernet(new_iface)),
+                            iface.current.clone(),
+                        ) {
+                            pf_ifaces.push(new_merged_iface);
+                            apply_iface.ethernet = None;
+                        }
+                    }
+                }
+            }
+        }
+
+        if pf_ifaces.is_empty() {
+            None
+        } else {
+            let mut pf_state = MergedNetworkState::default();
+            for pf_iface in pf_ifaces {
+                pf_state.interfaces.insert_order.push((
+                    pf_iface.merged.name().to_string(),
+                    pf_iface.merged.iface_type(),
+                ));
+                pf_state
+                    .interfaces
+                    .kernel_ifaces
+                    .insert(pf_iface.merged.name().to_string(), pf_iface);
+            }
+            Some(pf_state)
+        }
     }
 }

rust/src/lib/query_apply/net_state.rs
@@ -87,7 +87,7 @@ impl NetworkState {
         cur_net_state.set_include_secrets(true);
         cur_net_state.retrieve_full()?;
 
-        let merged_state = MergedNetworkState::new(
+        let mut merged_state = MergedNetworkState::new(
             self.clone(),
             cur_net_state.clone(),
             false,
@@ -106,62 +106,57 @@ impl NetworkState {
         }
 
         if !self.kernel_only {
-            let retry_count = if ifaces.has_sriov_enabled() {
-                VERIFY_RETRY_COUNT_SRIOV
-            } else {
-                VERIFY_RETRY_COUNT
-            };
-
-            let timeout = self.timeout.unwrap_or(DEFAULT_ROLLBACK_TIMEOUT);
-            let checkpoint = nm_checkpoint_create(timeout)?;
-            log::info!("Created checkpoint {}", &checkpoint);
-
-            with_nm_checkpoint(&checkpoint, self.no_commit, || {
-                // NM might have unknown race problem found by verify stage,
-                // we try to apply the state again if so.
-                with_retry(
-                    VERIFY_RETRY_INTERVAL_MILLISECONDS,
-                    VERIFY_RETRY_NM,
-                    || {
-                        nm_checkpoint_timeout_extend(&checkpoint, timeout)?;
-                        nm_apply(&merged_state, &checkpoint, timeout)?;
-                        if merged_state.is_global_ovsdb_changed()
-                            && ovsdb_is_running()
-                        {
-                            ovsdb_apply(&merged_state)?;
-                        }
-                        if let Some(running_hostname) = self
-                            .hostname
-                            .as_ref()
-                            .and_then(|c| c.running.as_ref())
-                        {
-                            set_running_hostname(running_hostname)?;
-                        }
-                        if !self.no_verify {
-                            with_retry(
-                                VERIFY_RETRY_INTERVAL_MILLISECONDS,
-                                retry_count,
-                                || {
-                                    nm_checkpoint_timeout_extend(
-                                        &checkpoint,
-                                        timeout,
-                                    )?;
-                                    let mut new_cur_net_state =
-                                        cur_net_state.clone();
-                                    new_cur_net_state.set_include_secrets(true);
-                                    new_cur_net_state.retrieve_full()?;
-                                    merged_state.verify(&new_cur_net_state)
-                                },
-                            )
-                        } else {
-                            Ok(())
-                        }
-                    },
-                )
-            })
+            self.apply_with_nm_backend(&mut merged_state, &cur_net_state)
         } else {
             // TODO: Need checkpoint for kernel only mode
-            nispor_apply(&merged_state)?;
+            self.apply_without_nm_backend(&merged_state, &cur_net_state)
+        }
+    }
+
+    fn apply_with_nm_backend(
+        &self,
+        merged_state: &mut MergedNetworkState,
+        cur_net_state: &Self,
+    ) -> Result<(), NmstateError> {
+        let pf_merged_state = merged_state.isolate_sriov_conf_out();
+        let timeout = self.timeout.unwrap_or(DEFAULT_ROLLBACK_TIMEOUT);
+        let checkpoint = nm_checkpoint_create(timeout)?;
+        log::info!("Created checkpoint {}", &checkpoint);
+
+        with_nm_checkpoint(&checkpoint, self.no_commit, || {
+            if let Some(pf_merged_state) = pf_merged_state {
+                self.apply_with_nm_backend_and_under_checkpoint(
+                    &pf_merged_state,
+                    cur_net_state,
+                    &checkpoint,
+                    VERIFY_RETRY_COUNT_SRIOV,
+                )?
+            }
+            self.apply_with_nm_backend_and_under_checkpoint(
+                merged_state,
+                cur_net_state,
+                &checkpoint,
+                VERIFY_RETRY_COUNT,
+            )
+        })
+    }
+
+    fn apply_with_nm_backend_and_under_checkpoint(
+        &self,
+        merged_state: &MergedNetworkState,
+        cur_net_state: &Self,
+        checkpoint: &str,
+        retry_count: usize,
+    ) -> Result<(), NmstateError> {
+        let timeout = self.timeout.unwrap_or(DEFAULT_ROLLBACK_TIMEOUT);
+        // NM might have unknown race problem found by verify stage,
+        // we try to apply the state again if so.
+        with_retry(VERIFY_RETRY_INTERVAL_MILLISECONDS, VERIFY_RETRY_NM, || {
+            nm_checkpoint_timeout_extend(checkpoint, timeout)?;
+            nm_apply(merged_state, checkpoint, timeout)?;
+            if merged_state.is_global_ovsdb_changed() && ovsdb_is_running() {
+                ovsdb_apply(merged_state)?;
+            }
             if let Some(running_hostname) =
                 self.hostname.as_ref().and_then(|c| c.running.as_ref())
             {
@@ -170,9 +165,11 @@ impl NetworkState {
             if !self.no_verify {
                 with_retry(
                     VERIFY_RETRY_INTERVAL_MILLISECONDS,
-                    VERIFY_RETRY_COUNT_KERNEL_MODE,
+                    retry_count,
                     || {
+                        nm_checkpoint_timeout_extend(checkpoint, timeout)?;
                         let mut new_cur_net_state = cur_net_state.clone();
+                        new_cur_net_state.set_include_secrets(true);
                         new_cur_net_state.retrieve_full()?;
                         merged_state.verify(&new_cur_net_state)
                     },
@@ -180,6 +177,32 @@ impl NetworkState {
             } else {
                 Ok(())
             }
+        })
+    }
+
+    fn apply_without_nm_backend(
+        &self,
+        merged_state: &MergedNetworkState,
+        cur_net_state: &Self,
+    ) -> Result<(), NmstateError> {
+        nispor_apply(merged_state)?;
+        if let Some(running_hostname) =
+            self.hostname.as_ref().and_then(|c| c.running.as_ref())
+        {
+            set_running_hostname(running_hostname)?;
+        }
+        if !self.no_verify {
+            with_retry(
+                VERIFY_RETRY_INTERVAL_MILLISECONDS,
+                VERIFY_RETRY_COUNT_KERNEL_MODE,
+                || {
+                    let mut new_cur_net_state = cur_net_state.clone();
+                    new_cur_net_state.retrieve_full()?;
+                    merged_state.verify(&new_cur_net_state)
+                },
+            )
+        } else {
+            Ok(())
         }
     }
 

tests/integration/sriov_test.py
@@ -9,6 +9,8 @@ import libnmstate
 from libnmstate.schema import Bond
 from libnmstate.schema import Ethernet
 from libnmstate.schema import Interface
+from libnmstate.schema import InterfaceIPv4
+from libnmstate.schema import InterfaceIPv6
 from libnmstate.schema import InterfaceState
 from libnmstate.schema import InterfaceType
 from libnmstate.schema import LinuxBridge
@@ -430,3 +432,37 @@ class TestSrIov:
         libnmstate.apply(desired_state)
 
         assertlib.assert_state_match(expected_state)
+
+    def test_enable_sriov_and_use_future_vf(self, disable_sriov):
+        pf_name = _test_nic_name()
+        iface_infos = [
+            {
+                Interface.NAME: pf_name,
+                Interface.STATE: InterfaceState.UP,
+                Ethernet.CONFIG_SUBTREE: {
+                    Ethernet.SRIOV_SUBTREE: {Ethernet.SRIOV.TOTAL_VFS: 2},
+                },
+            },
+            {
+                Interface.NAME: f"sriov:{pf_name}:0",
+                Interface.STATE: InterfaceState.UP,
+                Interface.IPV4: {
+                    InterfaceIPv4.ENABLED: False,
+                },
+                Interface.IPV6: {
+                    InterfaceIPv6.ENABLED: False,
+                },
+            },
+            {
+                Interface.NAME: f"sriov:{pf_name}:1",
+                Interface.STATE: InterfaceState.UP,
+                Interface.IPV4: {
+                    InterfaceIPv4.ENABLED: False,
+                },
+                Interface.IPV6: {
+                    InterfaceIPv6.ENABLED: False,
+                },
+            },
+        ]
+        desired_state = {Interface.KEY: iface_infos}
+        libnmstate.apply(desired_state)

rust/src/lib/ifaces/ethernet.rs
@@ -166,6 +166,17 @@ pub struct VethConfig {
 }
 
 impl MergedInterfaces {
+    pub(crate) fn has_sriov_vf_changes(&self) -> bool {
+        self.kernel_ifaces.values().any(|i| {
+            if let Some(Interface::Ethernet(eth_iface)) = i.for_apply.as_ref() {
+                eth_iface.ethernet.as_ref().map(|e| e.sr_iov.is_some())
+                    == Some(true)
+            } else {
+                false
+            }
+        })
+    }
+
     // Raise error if new veth interface has no peer defined.
     // Mark old veth peer as absent when veth changed its peer.
     // Mark veth peer as absent also when veth is marked as absent.
@@ -194,6 +205,7 @@ impl MergedInterfaces {
                 if eth_iface.veth.is_none()
                     && !self.gen_conf_mode
                     && !veth_peers.contains(&eth_iface.base.name.as_str())
+                    && !self.has_sriov_vf_changes()
                 {
                     return Err(NmstateError::new(
                         ErrorKind::InvalidArgument,

rust/src/lib/query_apply/inter_ifaces.rs
@@ -57,16 +57,6 @@ impl Interfaces {
         }
     }
 
-    pub(crate) fn has_sriov_enabled(&self) -> bool {
-        self.kernel_ifaces.values().any(|i| {
-            if let Interface::Ethernet(eth_iface) = i {
-                eth_iface.sriov_is_enabled()
-            } else {
-                false
-            }
-        })
-    }
-
     pub(crate) fn hide_controller_prop(&mut self) {
         for iface in self.kernel_ifaces.values_mut() {
             iface.base_iface_mut().controller = None;

rust/src/lib/unit_tests/sriov.rs
@@ -2,8 +2,9 @@
 
 use crate::{
     unit_tests::testlib::new_eth_iface, BridgePortVlanMode, ErrorKind,
-    EthernetConfig, Interface, InterfaceType, Interfaces, MergedInterfaces,
-    SrIovConfig, SrIovVfConfig,
+    EthernetConfig, EthernetDuplex, Interface, InterfaceType, Interfaces,
+    MergedInterfaces, MergedNetworkState, NetworkState, SrIovConfig,
+    SrIovVfConfig,
 };
 
 #[test]
@@ -585,4 +586,79 @@ fn test_sriov_vf_auto_fill_vf_conf() {
         )
         .unwrap()
     );
+}
+
+#[test]
+fn test_sriov_enable_and_use_in_single_yaml() {
+    let desired = serde_yaml::from_str::<NetworkState>(
+        r#"---
+        interfaces:
+        - name: eth1
+          type: ethernet
+          state: up
+          ethernet:
+            speed: 10000
+            duplex: full
+            auto-negotiation: false
+            sr-iov:
+              total-vfs: 2
+        - name: eth1v0
+          type: ethernet
+          state: up
+        - name: eth1v1
+          type: ethernet
+          state: up
+        "#,
+    )
+    .unwrap();
+
+    let current = serde_yaml::from_str::<NetworkState>(
+        r#"---
+        interfaces:
+        - name: eth1
+          type: ethernet
+          state: up
+          ethernet:
+            sr-iov:
+              total-vfs: 0
+        "#,
+    )
+    .unwrap();
+
+    let mut merged_state =
+        MergedNetworkState::new(desired, current, false, false).unwrap();
+
+    let pf_state = merged_state.isolate_sriov_conf_out().unwrap();
+
+    if let Interface::Ethernet(pf_iface) = pf_state
+        .interfaces
+        .kernel_ifaces
+        .get("eth1")
+        .unwrap()
+        .for_apply
+        .as_ref()
+        .unwrap()
+    {
+        let eth_conf = pf_iface.ethernet.as_ref().unwrap();
+        assert_eq!(eth_conf.auto_neg, Some(false));
+        assert_eq!(eth_conf.speed, Some(10000));
+        assert_eq!(eth_conf.duplex, Some(EthernetDuplex::Full));
+        let sr_iov_conf = eth_conf.sr_iov.as_ref().unwrap();
+        assert_eq!(sr_iov_conf.total_vfs, Some(2));
+    } else {
+        panic!("Expecting Ethernet interface, got {:?}", pf_state);
+    }
+    if let Interface::Ethernet(second_pf_iface) = merged_state
+        .interfaces
+        .kernel_ifaces
+        .get("eth1")
+        .unwrap()
+        .for_apply
+        .as_ref()
+        .unwrap()
+    {
+        assert!(second_pf_iface.ethernet.is_none())
+    } else {
+        panic!("Expecting Ethernet interface, got {:?}", pf_state);
+    }
 }

commit e639a384e0288b67327b79e884ca69cefe089902

sriov: Fix regression on waiting SRIOV
The 20871bac introduced a regression causing SR-IOV verification error
when enabling large mount(60+) VF.

The root cause is we only retry verification
`VERIFY_RETRY_COUNT_SRIOV(60)` times when VF changed with missing ethernet
VF. Fixed by use `VERIFY_RETRY_COUNT_SRIOV` when we have VF count
changed.

In test with ixgbe, we noticed 60 seconds is not enough, hence increase
to 300 seconds for verification retry.

Integration test case included and manually tested on ixgbe card.

Signed-off-by: Gris Ge <fge@redhat.com>

rust/src/lib/query_apply/ethernet.rs
@@ -116,7 +116,7 @@ impl NetworkState {
         self.has_vf_count_change(current) && self.has_missing_eth(current)
     }
 
-    fn has_vf_count_change(&self, current: &Self) -> bool {
+    pub(crate) fn has_vf_count_change(&self, current: &Self) -> bool {
         for iface in
             self.interfaces.kernel_ifaces.values().filter(|i| i.is_up())
         {

rust/src/lib/query_apply/net_state.rs
@@ -13,7 +13,7 @@ use crate::{
 const DEFAULT_ROLLBACK_TIMEOUT: u32 = 60;
 const VERIFY_RETRY_INTERVAL_MILLISECONDS: u64 = 1000;
 const VERIFY_RETRY_COUNT: usize = 5;
-const VERIFY_RETRY_COUNT_SRIOV: usize = 60;
+const VERIFY_RETRY_COUNT_SRIOV: usize = 300;
 const VERIFY_RETRY_COUNT_KERNEL_MODE: usize = 5;
 const RETRY_NM_COUNT: usize = 2;
 const RETRY_NM_INTERVAL_MILLISECONDS: u64 = 2000;
@@ -123,7 +123,16 @@ impl NetworkState {
                 None
             };
 
-        let timeout = self.timeout.unwrap_or(DEFAULT_ROLLBACK_TIMEOUT);
+        let mut timeout = self.timeout.unwrap_or(DEFAULT_ROLLBACK_TIMEOUT);
+        let verify_count = if self.has_vf_count_change(&cur_net_state) {
+            timeout = VERIFY_RETRY_COUNT_SRIOV as u32
+                * VERIFY_RETRY_INTERVAL_MILLISECONDS as u32
+                / 1000;
+            VERIFY_RETRY_COUNT_SRIOV
+        } else {
+            VERIFY_RETRY_COUNT
+        };
+
         let checkpoint = match nm_checkpoint_create(timeout) {
             Ok(c) => c,
             Err(e) => {
@@ -141,12 +150,6 @@ impl NetworkState {
 
         log::info!("Created checkpoint {}", &checkpoint);
 
-        let verify_count = if pf_state.is_some() {
-            VERIFY_RETRY_COUNT_SRIOV
-        } else {
-            VERIFY_RETRY_COUNT
-        };
-
         with_nm_checkpoint(&checkpoint, self.no_commit, || {
             if let Some(pf_state) = pf_state {
                 let pf_merged_state = MergedNetworkState::new(
@@ -160,6 +163,7 @@ impl NetworkState {
                     &cur_net_state,
                     &checkpoint,
                     verify_count,
+                    timeout,
                 )?;
                 // Refresh current state
                 cur_net_state.retrieve()?;
@@ -178,6 +182,7 @@ impl NetworkState {
                 &cur_net_state,
                 &checkpoint,
                 verify_count,
+                timeout,
             )
         })
     }
@@ -188,8 +193,8 @@ impl NetworkState {
         cur_net_state: &Self,
         checkpoint: &str,
         retry_count: usize,
+        timeout: u32,
     ) -> Result<(), NmstateError> {
-        let timeout = self.timeout.unwrap_or(DEFAULT_ROLLBACK_TIMEOUT);
         // NM might have unknown race problem found by verify stage,
         // we try to apply the state again if so.
         with_retry(RETRY_NM_INTERVAL_MILLISECONDS, RETRY_NM_COUNT, || {

tests/integration/sriov_test.py
@@ -121,6 +121,31 @@ def sriov_iface_vf(disable_sriov):
     yield desired_state
 
 
+@pytest.fixture
+def sriov_with_62_vfs():
+    pf_name = _test_nic_name()
+    iface_info = {
+        Interface.NAME: pf_name,
+        Interface.STATE: InterfaceState.UP,
+        Ethernet.CONFIG_SUBTREE: {
+            Ethernet.SRIOV_SUBTREE: {Ethernet.SRIOV.TOTAL_VFS: 62},
+        },
+    }
+    desired_state = {Interface.KEY: [iface_info]}
+    libnmstate.apply(desired_state)
+    yield
+    libnmstate.apply(
+        {
+            Interface.KEY: [
+                {
+                    Interface.NAME: pf_name,
+                    Interface.STATE: InterfaceState.ABSENT,
+                }
+            ]
+        }
+    )
+
+
 @pytest.mark.skipif(
     not os.environ.get("TEST_REAL_NIC"),
     reason="Need to define TEST_REAL_NIC for SR-IOV test",
@@ -460,4 +485,19 @@ class TestSrIov:
             },
         ]
         desired_state = {Interface.KEY: iface_infos}
+        libnmstate.apply(desired_state)
+
+    # Changing VF from 62 to 63 require massive time as kernel require us
+    # to disable SRIOV before changing VF count, this test is focus on
+    # whether nmstate has enough verification retry.
+    def test_change_vf_from_62_to_63(self, sriov_with_62_vfs):
+        pf_name = _test_nic_name()
+        iface_info = {
+            Interface.NAME: pf_name,
+            Interface.STATE: InterfaceState.UP,
+            Ethernet.CONFIG_SUBTREE: {
+                Ethernet.SRIOV_SUBTREE: {Ethernet.SRIOV.TOTAL_VFS: 63},
+            },
+        }
+        desired_state = {Interface.KEY: [iface_info]}
         libnmstate.apply(desired_state)