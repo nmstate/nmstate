eb80a11be00dd22fccff997f7042753c12f2015f
Gris Ge <fge@redhat.com>
2023-09-30 11:26:37 +0800

    test: Remove verify_change=False for absent action
    For test `test_create_ovs_with_internal_ports_in_reverse_order` which
    check absent of interface right after the `state: absent` applied,
    if we use `verify_change=False`, nmstate will not wait till interface
    gone.
    
    This patch removed all `verify_change=False` for absent action.
    
    Signed-off-by: Gris Ge <fge@redhat.com>

--- tests/integration/testlib/dummy.py
+++ tests/integration/testlib/dummy.py
@@ -27,7 +27,6 @@ def nm_unmanaged_dummy(name):
                         }
                     ]
                 },
-                verify_change=False,
             )
         except Exception:
             # dummy1 might not became managed by NM, hence removal might fail
