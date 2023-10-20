845c3213e51aef966f6315ab17529d41eb8b22c6
Gris Ge <fge@redhat.com>
2023-10-20 13:07:05 +0800

    rpm: Fix `make rpm` failure in Fedora
    Previously, without fix on SONAME[1], when installing rpm generated by
    `make rpm`, Fedora will complains:
    
        error: Failed dependencies:
                libnmstate.so()(64bit) is needed by nmstate-devel
    
    To fix the issue, we updated the nmstate.spec.in to hold:
        * Both Fedora and RHEL specific lines
        * Both official release and snapshot build(snapshot will use
          internet for crates.io access)
    
    Tested on CentOS stream 9, Fedora 38 and Fedora Rawhide(40).
    
    Also removed copr build for RHEL/CentOS 8 due to no usage.
    
    Resolves: RHEL-5002
    
    [1]: https://github.com/rust-lang/cargo/issues/5045
    
    Signed-off-by: Gris Ge <fge@redhat.com>

--- .github/workflows/build_rpm.sh
+++ .github/workflows/build_rpm.sh
@@ -11,14 +11,14 @@ CONTAINER_C9S_IMG="quay.io/nmstate/c9s-nmstate-build"
 if [ $1 == "el8" ];then
     podman run -v $PROJECT_PATH:$CONTAINER_WORKSPACE:rw \
         $CONTAINER_C8S_IMG /bin/bash -c \
-        "cd $CONTAINER_WORKSPACE && SKIP_VENDOR_CREATION=1 make rpm"
+        "cd $CONTAINER_WORKSPACE && make rpm"
 
     mkdir -p $OUTPUT_DIR/el8 || true
     mv -v *.rpm $OUTPUT_DIR/el8/
 else
     podman run -v $PROJECT_PATH:$CONTAINER_WORKSPACE:rw \
         $CONTAINER_C9S_IMG /bin/bash -c \
-        "cd $CONTAINER_WORKSPACE && SKIP_VENDOR_CREATION=1 make rpm"
+        "cd $CONTAINER_WORKSPACE && make rpm"
 
     mkdir -p $OUTPUT_DIR/el9 || true
     mv -v *.rpm $OUTPUT_DIR/el9/
