d7aa837dee97537e7380ddc1a67175733854e9fb
beekaz <sooperfect00@gmail.com>
2024-03-28 14:37:45 -0400

    Implement std::fmt::Debug for NetworkState and its subtypes
    Signed-off-by: beekaz <sooperfect00@gmail.com>

--- rust/src/lib/unit_tests/net_state.rs
+++ rust/src/lib/unit_tests/net_state.rs
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: Apache-2.0
 
+use crate::HostNameState;
+use crate::Interfaces;
 use crate::NetworkState;
+use crate::OvnConfiguration;
+use crate::RouteRules;
+use crate::Routes;
 
 #[test]
 fn test_invalid_top_key() {
@@ -22,4 +27,44 @@ fn test_invalid_top_type() {
     );
 
     assert!(result.is_err());
+}
+
+#[test]
+fn test_network_state_debug() {
+    let network_state = NetworkState {
+        hostname: Some(HostNameState::default()), // Use Option<HostNameState>
+        dns: None,
+        rules: RouteRules::default(),
+        routes: Routes::default(),
+        interfaces: Interfaces::default(),
+        ovsdb: None,
+        timeout: Some(60),
+        no_verify: true,
+        no_commit: false,
+        include_secrets: true,
+        include_status_data: false,
+        kernel_only: true,
+        memory_only: true,
+        running_config_only: false,
+        ovn: OvnConfiguration::default(),
+    };
+
+    let debug_output = format!("{:?}", network_state);
+
+    // Assert that the debug output contains the expected fields
+    assert!(debug_output.contains("hostname"));
+    assert!(debug_output.contains("dns"));
+    assert!(debug_output.contains("rules"));
+    assert!(debug_output.contains("routes"));
+    assert!(debug_output.contains("interfaces"));
+    assert!(debug_output.contains("ovsdb"));
+    assert!(debug_output.contains("timeout"));
+    assert!(debug_output.contains("no_verify"));
+    assert!(debug_output.contains("no_commit"));
+    assert!(debug_output.contains("include_secrets"));
+    assert!(debug_output.contains("include_status_data"));
+    assert!(debug_output.contains("running_config_only"));
+    assert!(debug_output.contains("memory_only"));
+    // Assert that the debug output contains the hidden password
+    assert!(debug_output.contains("<_password_hid_by_nmstate>"))
 }
