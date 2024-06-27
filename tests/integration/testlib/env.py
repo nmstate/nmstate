04d39130ed75529ed04e1b7b27f2ac5544f75e26
Gris Ge <fge@redhat.com>
2024-06-27 21:22:22 +0800

    test: Remove the need of python3-gobject-base
    The python3-gobject-base is too big for test container.
    Change the test code to use rpm instead to query NetworkManager version.
    
    Signed-off-by: Gris Ge <fge@redhat.com>

--- tests/integration/testlib/env.py
+++ tests/integration/testlib/env.py
@@ -2,18 +2,7 @@
 
 import os
 
-import gi
-
 from .cmdlib import exec_cmd
-
-gi.require_version("NM", "1.0")
-# It is required to state the NM version before importing it
-# But this break the flak8 rule: https://www.flake8rules.com/rules/E402.html
-# Use NOQA: E402 to suppress it.
-# pylint: disable=no-name-in-module
-from gi.repository import NM  # NOQA: E402
-
-# pylint: enable=no-name-in-module
 
 
 def is_fedora():
@@ -24,12 +13,12 @@ def is_ubuntu_kernel():
     return "Ubuntu" in os.uname().version
 
 
-def nm_major_minor_version():
-    return float(f"{NM.MAJOR_VERSION}.{NM.MINOR_VERSION}")
-
-
 def nm_minor_version():
-    return int(f"{NM.MINOR_VERSION}")
+    version_str = exec_cmd(
+        "rpm -q NetworkManager --qf %{VERSION}".split(),
+        check=True,
+    )[1]
+    return int(version_str.split(".")[1])
 
 
 def is_k8s():
