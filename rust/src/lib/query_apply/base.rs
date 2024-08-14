9e7d9d2bfe40806082e393642049da3c78cb341a
Gris Ge <fge@redhat.com>
2024-08-14 22:30:33 +0800

    gen_diff: Include context information for `gen_diff()`
    When user applying below policy file:
    
    ```yml
    ---
    capture:
      gw: routes.running.destination=="0.0.0.0/0"
      gw-iface: interfaces.name==capture.gw.routes.running.0.next-hop-interface
    desiredState:
      interfaces:
        - name: "{{ capture.gw-iface.interfaces.0.name }}"
          identifier: mac-address
          mac-address: "{{ capture.gw-iface.interfaces.0.mac-address }}"
    ```
    
    Nmstate generate a `NetworkState` without mac address defined because it
    is not changed.
    
    To fix the issue, we should treat `mac-address` as context of
    `interface.identifier: mac-address` which should be included in
    `gen_diff()` output.
    
    Unit test case included.
    
    Resolves: https://issues.redhat.com/browse/RHEL-54292
    
    Signed-off-by: Gris Ge <fge@redhat.com>

--- rust/src/lib/query_apply/base.rs
+++ rust/src/lib/query_apply/base.rs
@@ -1,8 +1,19 @@
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::{BaseInterface, InterfaceState, InterfaceType, OvsDbIfaceConfig};
+use crate::{
+    BaseInterface, InterfaceIdentifier, InterfaceState, InterfaceType,
+    OvsDbIfaceConfig,
+};
 
 impl BaseInterface {
+    pub(crate) fn include_diff_context(&mut self, current: &Self) {
+        if self.identifier == Some(InterfaceIdentifier::MacAddress)
+            && self.mac_address.is_none()
+        {
+            self.mac_address.clone_from(&current.mac_address)
+        }
+    }
+
     pub(crate) fn sanitize_current_for_verify(&mut self) {
         if self.controller.is_none() {
             self.controller = Some(String::new());
