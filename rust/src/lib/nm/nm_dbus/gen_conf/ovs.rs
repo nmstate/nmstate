e96be246863f7ecc007df53f0754d89c8c05bfd1
Koen de Laat <koen.de.laat@philips.com>
2024-10-09 17:31:52 +0200

    gc: Fix trunk vlans for ovs port in gen_conf
    The `nmstatectl gc` will fail on OVS trunk VLAN state like:
    
    ```yml
    ---
    interfaces:
      - name: br0
        type: ovs-bridge
        state: up
        bridge:
          port:
            - name: eth1
              vlan:
                mode: trunk
                trunk-tags:
                - id: 500
    ```
    
    Integration test case included.
    
    Signed-off-by: Koen de Laat <koen.de.laat@philips.com>

--- rust/src/lib/nm/nm_dbus/gen_conf/ovs.rs
+++ rust/src/lib/nm/nm_dbus/gen_conf/ovs.rs
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
+use crate::nm::nm_dbus::ToDbusValue;
 use std::collections::HashMap;
 
 use super::super::{
@@ -9,10 +10,40 @@ use super::super::{
 };
 
 impl ToKeyfile for NmSettingOvsBridge {}
-impl ToKeyfile for NmSettingOvsPort {}
 impl ToKeyfile for NmSettingOvsIface {}
 impl ToKeyfile for NmSettingOvsPatch {}
 impl ToKeyfile for NmSettingOvsDpdk {}
+
+impl ToKeyfile for NmSettingOvsPort {
+    fn to_keyfile(&self) -> Result<HashMap<String, zvariant::Value>, NmError> {
+        let mut ret = HashMap::new();
+
+        for (k, v) in self.to_value()?.drain() {
+            if k != "trunks" {
+                ret.insert(k.to_string(), v);
+            }
+        }
+        if let Some(vlans) = self.trunks.as_ref() {
+            let mut vlans_clone = vlans.clone();
+            vlans_clone.sort_unstable_by_key(|v| v.start);
+            let mut vlans_str = Vec::new();
+            for vlan in vlans_clone {
+                let ret = if vlan.start == vlan.end {
+                    vlan.start.to_string()
+                } else {
+                    format!("{}-{}", vlan.start, vlan.end)
+                };
+                vlans_str.push(ret);
+            }
+            ret.insert(
+                "trunks".to_string(),
+                zvariant::Value::new(vlans_str.join(",")),
+            );
+        }
+
+        Ok(ret)
+    }
+}
 
 impl ToKeyfile for NmSettingOvsExtIds {
     fn to_keyfile(&self) -> Result<HashMap<String, zvariant::Value>, NmError> {
