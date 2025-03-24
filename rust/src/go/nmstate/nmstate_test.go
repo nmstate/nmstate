326264eda3665900d85f2f8de28a3429e67f728a
Wen Liang <liangwen12year@gmail.com>
2025-03-23 20:35:26 -0400

    go: add generating state from policy support to golang bindings
    Introduce GenerateStateFromPolicy in the Golang bindings to validate
    YAML files containing nmpolicy. This function behaves similarly to
    GenerateConfiguration for nmstate YAML, propagating the C binding errors
    back to the user.
    
    Resolves: https://issues.redhat.com/browse/RHEL-69925
    
    Signed-off-by: Wen Liang <liangwen12year@gmail.com>

--- rust/src/go/nmstate/nmstate_test.go
+++ rust/src/go/nmstate/nmstate_test.go
@@ -74,3 +74,61 @@ func TestGenerateConfiguration(t *testing.T) {
 	assert.NoError(t, err, "must succeed calling nmstate_generate_configurations c binding")
 	assert.NotEmpty(t, config, "config should not be empty")
 }
+
+func TestGenerateStateFromPolicy(t *testing.T) {
+	nms := New()
+	// Define a sample policy and current state
+	policy := `{
+		"capture": {
+			"ethernets": "interfaces.type==\"ethernet\"",
+			"ethernets-up": "capture.ethernets.interfaces.state==\"up\"",
+			"ethernets-lldp": "capture.ethernets-up | interfaces.lldp.enabled:=true"
+		},
+		"desiredState": {
+			"interfaces": "{{ capture.ethernets-lldp.interfaces }}"
+		}
+	}`
+
+	currentState := `{
+		"interfaces": [
+			{
+				"name": "eth0",
+				"type": "ethernet",
+				"state": "up",
+				"mac-address": "52:55:00:D1:55:01",
+				"accept-all-mac-addresses": false,
+				"lldp": {
+					"enabled": false
+				}
+			},
+			{
+				"name": "eth1",
+				"type": "ethernet",
+				"state": "down",
+				"mac-address": "52:55:00:D1:56:02",
+				"accept-all-mac-addresses": false,
+				"lldp": {
+					"enabled": false
+				}
+			},
+			{
+				"name": "eth2",
+				"type": "ethernet",
+				"state": "down",
+				"mac-address": "52:55:00:D1:56:04",
+				"accept-all-mac-addresses": false
+			},
+			{
+				"name": "eth4",
+				"type": "ethernet",
+				"state": "up",
+				"mac-address": "52:55:00:D1:57:03",
+				"accept-all-mac-addresses": false
+			}
+		]
+	}`
+
+	state, err := nms.GenerateStateFromPolicy(policy, currentState)
+	assert.NoError(t, err, "must succeed calling nmstate_net_state_from_policy c binding")
+	assert.NotEmpty(t, state, "state should not be empty")
+}
