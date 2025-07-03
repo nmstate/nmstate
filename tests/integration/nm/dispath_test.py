d21ac4387d9c5eeb0f4cd8565e9de9e39ae18bf3
Gris Ge <fge@redhat.com>
2025-07-03 11:49:49 +0800

    nm dispatch: Fix race when modifying dispatch script
    Previously we reapply/activate the NmConnection before modifying the
    dispatcher script, this would cause NetworkManager invoked old
    dispatcher script content.
    
    Fixed by modify the dispatcher script before NmConnection actions.
    
    Integration test case included.
    
    Resolves: https://issues.redhat.com/browse/RHEL-94840
    
    Signed-off-by: Gris Ge <fge@redhat.com>

--- tests/integration/nm/dispath_test.py
+++ tests/integration/nm/dispath_test.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
+from contextlib import suppress
 import os
 
 import pytest
@@ -28,9 +29,8 @@ def assert_dispatch_script_not_exist(iface_name, action):
 
 @pytest.fixture
 def eth1_up_with_dispatch_script(eth1_up):
-    libnmstate.apply(
-        load_yaml(
-            """---
+    state = load_yaml(
+        """---
             interfaces:
             - name: eth1
               dispatch:
@@ -39,8 +39,9 @@ def eth1_up_with_dispatch_script(eth1_up):
                 post-deactivation: |
                   echo post-down-eth1 | systemd-cat
             """
-        ),
     )
+    libnmstate.apply(state)
+    yield state
 
 
 def test_add_and_remove_dispatch_script(eth1_up_with_dispatch_script):
@@ -76,7 +77,7 @@ def test_remove_dispatch_script_on_iface_absent(eth1_up):
     assert_dispatch_script_not_exist("eth1", "down")
 
 
-def test_modify_dispatch_script(eth1_up):
+def test_add_dispatch_script(eth1_up):
     libnmstate.apply(
         load_yaml(
             """---
@@ -94,3 +95,69 @@ def test_modify_dispatch_script(eth1_up):
     assert_dispatch_script(
         "eth1", "down", "echo new-post-down-eth1 | systemd-cat"
     )
+
+
+TEST_DISPATCH_STATE_FILE_ETH1 = "/tmp/nmstate_test_dispatch_up_eth1"
+TEST_DISPATCH_STATE_FILE_ETH2 = "/tmp/nmstate_test_dispatch_up_eth2"
+
+
+def assert_dispatch_state_files(expected_content):
+    with open(TEST_DISPATCH_STATE_FILE_ETH1) as fd:
+        content = fd.read()
+        assert content == expected_content
+
+    with open(TEST_DISPATCH_STATE_FILE_ETH2) as fd:
+        content = fd.read()
+        assert content == expected_content
+
+
+@pytest.fixture
+def delete_dispatch_state_file():
+    with suppress(FileNotFoundError):
+        os.remove(TEST_DISPATCH_STATE_FILE_ETH1)
+        os.remove(TEST_DISPATCH_STATE_FILE_ETH2)
+    yield
+    with suppress(FileNotFoundError):
+        os.remove(TEST_DISPATCH_STATE_FILE_ETH1)
+        os.remove(TEST_DISPATCH_STATE_FILE_ETH2)
+
+
+@pytest.fixture
+def eth1_eth2_with_old_dispatch_script(eth1_up, eth2_up):
+    state = load_yaml(
+        f"""---
+            interfaces:
+            - name: eth1
+              dispatch:
+                post-activation: |
+                  echo -n old > {TEST_DISPATCH_STATE_FILE_ETH1}
+            - name: eth2
+              dispatch:
+                post-activation: |
+                  echo -n old > {TEST_DISPATCH_STATE_FILE_ETH2}
+         """
+    )
+    libnmstate.apply(state)
+    assert_dispatch_state_files("old")
+
+
+def test_modify_dispatch_script(
+    delete_dispatch_state_file, eth1_eth2_with_old_dispatch_script
+):
+    libnmstate.apply(
+        load_yaml(
+            f"""---
+            interfaces:
+            - name: eth1
+              dispatch:
+                post-activation: |
+                  echo -n new > {TEST_DISPATCH_STATE_FILE_ETH1}
+            - name: eth2
+              dispatch:
+                post-activation: |
+                  echo -n new > {TEST_DISPATCH_STATE_FILE_ETH2}
+         """
+        ),
+    )
+
+    assert_dispatch_state_files("new")
