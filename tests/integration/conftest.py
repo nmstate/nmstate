f81dd068125d4c738c831a9e881d690270cb7e34
Gris Ge <fge@redhat.com>
2025-07-08 17:11:30 +0800

    test: ipsec: Rename `pull_ipsec_srv_container_image()` to `IpsecTestEnv.prepare()`
    Rename `pull_ipsec_srv_container_image()` to `IpsecTestEnv.prepare()` in
    `testlib/ipsec.py` so NMCI could consume.
    
    Signed-off-by: Gris Ge <fge@redhat.com>

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -18,7 +18,7 @@ from libnmstate.schema import RouteRule
 from .testlib import ifacelib
 from .testlib.veth import create_veth_pair
 from .testlib.veth import remove_veth_pair
-from .testlib.ipsec import pull_ipsec_srv_container_image
+from .testlib.ipsec import IpsecTestEnv
 
 
 REPORT_HEADER = """RPMs: {rpms}
@@ -94,7 +94,7 @@ def _mark_tier2_tests(items):
 
 @pytest.fixture(scope="session", autouse=True)
 def test_env_setup():
-    pull_ipsec_srv_container_image()
+    IpsecTestEnv.prepare()
     _logging_setup()
     old_state = libnmstate.show()
     old_state = _remove_interfaces_from_env(old_state)
