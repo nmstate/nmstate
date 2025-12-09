fd18b7eecda759f79c94442004e01baf6cd421fb
Gris Ge <fge@redhat.com>
2025-12-09 12:57:15 +0800

    Bump version to 2.2.58
    Signed-off-by: Gris Ge <fge@redhat.com>

--- rust/src/python/setup.py
+++ rust/src/python/setup.py
@@ -15,7 +15,7 @@ def requirements():
 
 setuptools.setup(
     name="nmstate",
-    version="2.2.57",
+    version="2.2.58",
     author="Gris Ge",
     author_email="fge@redhat.com",
     description="Python binding of nmstate",
