17e24040b1e2331deb9b5d4d0eda94e9008638e4
Gris Ge <fge@redhat.com>
2026-01-19 15:59:09 +0800

    Support referring interface name using its alternative name
    Example YAML:
    
    ```yml
    ---
    interfaces:
      - name: veryveryveryverylonglonglongname
        type: ethernet
        ipv4:
          address:
          - ip: 192.0.2.251
            prefix-length: 24
          dhcp: false
          enabled: true
        ipv6:
          enabled: false
    ```
    
    Will raise error if you have conflict interfaces settings like one
    interface refer to kernel name, another interface refer to alt-name.
    But it is OK to both interface marked as absent.
    
    We will not resolve the `interface.name` property to alt-name when the
    `interface.identifier` is defined and not `InterfaceIdentifier::Name`.
    
    Unit test cases and integration test cases included.
    
    Resolves: https://issues.redhat.com/browse/RHEL-126510
    
    Signed-off-by: Gris Ge <fge@redhat.com>

--- rust/src/lib/unit_tests/alt_name.rs
+++ rust/src/lib/unit_tests/alt_name.rs
@@ -136,3 +136,287 @@ fn test_alt_name_overbook() {
         assert!(e.msg().contains("overbook"));
     }
 }
+
+#[test]
+fn test_remove_iface_via_alt_name_ref() {
+    let desired: NetworkState = serde_yaml::from_str(
+        r"---
+        interfaces:
+          - name: veryveryveryverylonglonglongname
+            type: ethernet
+            state: absent",
+    )
+    .unwrap();
+    let current: NetworkState = serde_yaml::from_str(
+        r"---
+        interfaces:
+          - name: eth1
+            type: ethernet
+            mac-address: 52:54:00:15:17:63
+            alt-names:
+              - name: port1
+              - name: veryveryveryverylonglonglongname",
+    )
+    .unwrap();
+
+    let merged_state =
+        MergedNetworkState::new(desired, current, Default::default(), false)
+            .unwrap();
+
+    let merged_iface =
+        merged_state.interfaces.kernel_ifaces.get("eth1").unwrap();
+    let for_apply = merged_iface.for_apply.as_ref().unwrap();
+    let for_verify = merged_iface.for_verify.as_ref().unwrap();
+    let merged = &merged_iface.merged;
+
+    assert!(for_apply.is_absent());
+    assert!(for_verify.is_absent());
+    assert!(merged.is_absent());
+}
+
+#[test]
+fn test_change_iface_via_alt_name_ref() {
+    let desired: NetworkState = serde_yaml::from_str(
+        r"---
+        interfaces:
+          - name: veryveryveryverylonglonglongname
+            type: ethernet
+            state: up
+            ipv4:
+              enabled: true
+              dhcp: true",
+    )
+    .unwrap();
+    let current: NetworkState = serde_yaml::from_str(
+        r"---
+        interfaces:
+          - name: eth1
+            type: ethernet
+            mac-address: 52:54:00:15:17:63
+            alt-names:
+              - name: port1
+              - name: veryveryveryverylonglonglongname
+            ipv4:
+              enabled: false
+              ",
+    )
+    .unwrap();
+
+    let merged_state =
+        MergedNetworkState::new(desired, current, Default::default(), false)
+            .unwrap();
+
+    let merged_iface =
+        merged_state.interfaces.kernel_ifaces.get("eth1").unwrap();
+    let for_apply = merged_iface.for_apply.as_ref().unwrap();
+    let for_verify = merged_iface.for_verify.as_ref().unwrap();
+    let merged = &merged_iface.merged;
+
+    assert_eq!(
+        for_apply.base_iface().profile_name.as_deref(),
+        Some("veryveryveryverylonglonglongname")
+    );
+    assert_eq!(
+        for_apply.base_iface().ipv4.as_ref().map(|i| i.enabled),
+        Some(true)
+    );
+    assert_eq!(
+        for_apply.base_iface().ipv4.as_ref().and_then(|i| i.dhcp),
+        Some(true)
+    );
+
+    assert_eq!(
+        for_verify.base_iface().profile_name.as_deref(),
+        Some("veryveryveryverylonglonglongname")
+    );
+    assert_eq!(
+        for_verify.base_iface().ipv4.as_ref().map(|i| i.enabled),
+        Some(true)
+    );
+    assert_eq!(
+        for_verify.base_iface().ipv4.as_ref().and_then(|i| i.dhcp),
+        Some(true)
+    );
+
+    assert_eq!(
+        merged.base_iface().profile_name.as_deref(),
+        Some("veryveryveryverylonglonglongname")
+    );
+
+    assert_eq!(
+        merged.base_iface().ipv4.as_ref().map(|i| i.enabled),
+        Some(true)
+    );
+    assert_eq!(
+        merged.base_iface().ipv4.as_ref().and_then(|i| i.dhcp),
+        Some(true)
+    );
+}
+
+#[test]
+fn test_iface_ref_by_alt_name_but_desired_as_absent() {
+    let desired: NetworkState = serde_yaml::from_str(
+        r"---
+        interfaces:
+          - name: eth1
+            type: ethernet
+            state: absent
+          - name: veryveryveryverylonglonglongname
+            type: ethernet
+            state: up
+            ipv4:
+              enabled: true
+              dhcp: true",
+    )
+    .unwrap();
+    let current: NetworkState = serde_yaml::from_str(
+        r"---
+        interfaces:
+          - name: eth1
+            type: ethernet
+            mac-address: 52:54:00:15:17:63
+            alt-names:
+              - name: port1
+              - name: veryveryveryverylonglonglongname
+            ipv4:
+              enabled: false",
+    )
+    .unwrap();
+
+    let result =
+        MergedNetworkState::new(desired, current, Default::default(), false);
+
+    assert!(result.is_err());
+
+    if let Err(e) = result {
+        assert_eq!(e.kind(), ErrorKind::InvalidArgument);
+        assert!(e.msg().contains("eth1"));
+        assert!(e.msg().contains("veryveryveryverylonglonglongname"));
+        assert!(e.msg().contains("conflict"));
+    }
+}
+
+#[test]
+fn test_iface_ref_by_alt_name_but_conflict_desired() {
+    let desired: NetworkState = serde_yaml::from_str(
+        r"---
+        interfaces:
+          - name: eth1
+            type: ethernet
+            state: up
+          - name: veryveryveryverylonglonglongname
+            type: ethernet
+            state: up
+            ipv4:
+              enabled: true
+              dhcp: true",
+    )
+    .unwrap();
+    let current: NetworkState = serde_yaml::from_str(
+        r"---
+        interfaces:
+          - name: eth1
+            type: ethernet
+            mac-address: 52:54:00:15:17:63
+            alt-names:
+              - name: port1
+              - name: veryveryveryverylonglonglongname
+            ipv4:
+              enabled: false",
+    )
+    .unwrap();
+
+    let result =
+        MergedNetworkState::new(desired, current, Default::default(), false);
+
+    assert!(result.is_err());
+
+    if let Err(e) = result {
+        assert_eq!(e.kind(), ErrorKind::InvalidArgument);
+        assert!(e.msg().contains("eth1"));
+        assert!(e.msg().contains("veryveryveryverylonglonglongname"));
+        assert!(e.msg().contains("conflict"));
+    }
+}
+
+#[test]
+fn test_iface_ref_by_alt_name_ok_to_conflict_when_both_absent() {
+    let desired: NetworkState = serde_yaml::from_str(
+        r"---
+        interfaces:
+          - name: eth1
+            type: ethernet
+            state: absent
+          - name: veryveryveryverylonglonglongname
+            type: ethernet
+            state: absent",
+    )
+    .unwrap();
+    let current: NetworkState = serde_yaml::from_str(
+        r"---
+        interfaces:
+          - name: eth1
+            type: ethernet
+            mac-address: 52:54:00:15:17:63
+            alt-names:
+              - name: port1
+              - name: veryveryveryverylonglonglongname
+            ipv4:
+              enabled: false",
+    )
+    .unwrap();
+    let merged_state =
+        MergedNetworkState::new(desired, current, Default::default(), false)
+            .unwrap();
+
+    let merged_iface =
+        merged_state.interfaces.kernel_ifaces.get("eth1").unwrap();
+    let for_apply = merged_iface.for_apply.as_ref().unwrap();
+    let for_verify = merged_iface.for_verify.as_ref().unwrap();
+    let merged = &merged_iface.merged;
+
+    assert!(for_apply.is_absent());
+    assert!(for_verify.is_absent());
+    assert!(merged.is_absent());
+}
+
+#[test]
+fn test_do_not_resolve_mac_identifer_iface_to_alt_name() {
+    let desired: NetworkState = serde_yaml::from_str(
+        r"---
+        interfaces:
+          - name: port1
+            type: ethernet
+            identifier: mac-address
+            mac-address: 52:54:00:15:17:64
+            state: absent",
+    )
+    .unwrap();
+    let current: NetworkState = serde_yaml::from_str(
+        r"---
+        interfaces:
+          - name: eth1
+            type: ethernet
+            mac-address: 52:54:00:15:17:63
+            alt-names:
+              - name: port1
+              - name: veryveryveryverylonglonglongname
+            ipv4:
+              enabled: false
+          - name: eth2
+            type: ethernet
+            mac-address: 52:54:00:15:17:64
+            ipv4:
+              enabled: false",
+    )
+    .unwrap();
+    let merged_state =
+        MergedNetworkState::new(desired, current, Default::default(), false)
+            .unwrap();
+
+    let merged_iface =
+        merged_state.interfaces.kernel_ifaces.get("eth1").unwrap();
+    assert!(merged_iface.for_apply.is_none());
+    assert!(merged_iface.for_verify.is_none());
+    assert!(merged_iface.desired.is_none());
+}
