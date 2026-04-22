2ad0192ad3eb1abc124b15f6ef5711ce2c4245f5
Jan Vaclav <jvaclav@redhat.com>
2026-04-22 12:00:38 +0200

    release: refactor and add checks to verify prerequisites
    Signed-off-by: Jan Vaclav <jvaclav@redhat.com>

--- automation/upstream_release.sh
+++ automation/upstream_release.sh
@@ -1,8 +1,8 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 MAIN_BRANCH_NAME="base"
-UPSTERAM_GIT="https://github.com/nmstate/nmstate.git"
-NEW_RELEASE_URL="https://github.com/nmstate/nmstate/releases/new"
+UPSTREAM_GIT_HTTPS="https://github.com/nmstate/nmstate.git"
+UPSTREAM_GIT_SSH="git@github.com:nmstate/nmstate.git"
 TMP_CHANGELOG_FILE=$(mktemp)
 
 CHANGLOG_FORMAT="
@@ -25,6 +25,48 @@ CODE_BASE_DIR=$(readlink -f "$(dirname -- "$0")/..");
 
 cd $CODE_BASE_DIR;
 
+# Check prerequisites
+err=0
+for cmd in git hub cargo curl grep sed vim gpg cargo-vendor-filterer; do
+    if ! command -v $cmd > /dev/null 2>&1; then
+        echo "$cmd not found. Please install it." >&2
+        err=1
+    fi
+done
+if [ $err = 1 ]; then
+    exit 1
+fi
+
+if ! git fetch upstream 2>/dev/null; then
+    echo "Upstream repository not found. If you use ssh keys to push, add it using:" >&2
+    echo " $ git remote add upstream $UPSTREAM_GIT_SSH" >&2
+    echo "Otherwise, if you use HTTPS, add it using:" >&2
+    echo " $ git remote add upstream $UPSTREAM_GIT_HTTPS" >&2
+    exit 1
+fi
+
+CARGO_REGISTRY_TOKEN=$(grep -oP 'token = "\K[^"]+' ~/.cargo/credentials.toml || :)
+if [ -z "$CARGO_REGISTRY_TOKEN" ] || \
+   ! curl -s "https://crates.io/api/v1/crates?following=1" \
+          -H "Authorization: Bearer $CARGO_REGISTRY_TOKEN" | grep -q "perform this action"; then
+    # crates.io does not provide access to any normal API to query user status,
+    # so we (ab)use the crate search API to check if the token is valid.
+    echo "Cargo registry token is missing or invalid (may be expired). Please log in using:" >&2
+    echo " $ cargo login" >&2
+    echo "and confirm your email on crates.io: https://crates.io/settings/profile" >&2
+    echo "The token you use to log in should have at least 'publish-update' permissions." >&2
+    exit 1
+fi
+
+if ! hub pr list < /dev/null >/dev/null 2>&1; then
+    echo '"hub pr list" failed, this might mean your github login is invalid.' >&2
+    echo "To log in, you can run that command. As the password, you should use" >&2
+    echo "a GitHub personal access token with 'repo' scope." >&2
+    echo "You can make it here: https://github.com/settings/tokens/new" >&2
+    exit 1
+fi
+
+set -x
 CUR_VERSION=$(cat VERSION);
 CUR_MAJOR_VERSION=$(echo $CUR_VERSION|cut -f1 -d.)
 CUR_MINOR_VERSION=$(echo $CUR_VERSION|cut -f2 -d.)
@@ -36,8 +78,6 @@ NEXT_VERSION="${CUR_MAJOR_VERSION}.${CUR_MINOR_VERSION}.$(( \
 
 git branch new_release || true
 git checkout new_release
-git fetch upstream || (git remote add upstream $UPSTERAM_GIT; \
-    git fetch upstream)
 git reset --hard upstream/$MAIN_BRANCH_NAME
 echo "# Changelog" > $TMP_CHANGELOG_FILE
 echo "## [$CUR_VERSION] - $(date +%F)" >> $TMP_CHANGELOG_FILE
@@ -85,11 +125,6 @@ git tag --sign v$CUR_VERSION -m "New release ${CUR_VERSION}" \
     -m "$CHANGELOG_STR"
 git push upstream --tags
 
-cd $CODE_BASE_DIR/rust/src/lib
-cargo publish --allow-dirty
-cd $CODE_BASE_DIR/rust/src/cli
-cargo publish --allow-dirty
-
 cd $CODE_BASE_DIR
 RELEASE=1 make release
 
@@ -100,6 +135,16 @@ hub release create \
     -a nmstate-$CUR_VERSION.tar.gz -a nmstate-$CUR_VERSION.tar.gz.asc \
     -a nmstate-vendor-$CUR_VERSION.tar.xz \
     -F - "v$CUR_VERSION"
+
+publish_crate() {
+    cd $CODE_BASE_DIR/rust/src/$1
+    cargo publish --allow-dirty
+    cd $CODE_BASE_DIR
+}
+
+publish_crate lib
+publish_crate cli
+publish_crate clib
 
 # Bump version
 git branch bump_version || true
